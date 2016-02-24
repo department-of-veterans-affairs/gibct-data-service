3f038252c5bd3f20914111521244088fcdc65f8b
mphprogrammer <marc.harbatkin.va@gmail.com>
2016-02-23 21:37:59 -0500

    Post-Heroku commit
    Removed rainbow gem
    
    Linting errors ...
    
    Linting errors ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable and
-  # :registerable 
+  # :registerable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,33 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "When creating" do
-  	it "factory is valid" do
-  		expect(create :user).to be_valid
-  	end
+    it "factory is valid" do
+      expect(create(:user)).to be_valid
+    end
 
-  	it "needs an email" do
-  		expect{create :user, :no_email}.to raise_error(ActiveRecord::RecordInvalid)
-  	end
+    it "needs an email" do
+      expect { create(:user, :no_email) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
-  	it "needs a proper email" do
-  		expect{create :user, :bad_email}.to raise_error(ActiveRecord::RecordInvalid)
-  	end
+    it "needs a proper email" do
+      expect { create :user, :bad_email }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
-  	it "needs a va.gov email" do
-  		expect{create :user, :bad_email_domain}.to raise_error(ActiveRecord::RecordInvalid)
-  	end
+    it "needs a va.gov email" do
+      expect { create :user, :bad_email_domain }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
-  	it "needs a password" do
-  		expect{create :user, :no_password}.to raise_error(ActiveRecord::RecordInvalid)
-  	end
+    it "needs a password" do
+      expect { create :user, :no_password }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
-  	it "needs a password >= 8 characters" do
-  		expect{create :user, :short_password}.to raise_error(ActiveRecord::RecordInvalid)
-  	end  
+    it "needs a password >= 8 characters" do
+      expect { create :user, :short_password }.to raise_error(ActiveRecord::RecordInvalid)
+    end
 
-  	it "needs a password <= 72 characters" do
-  		expect{create :user, :long_password}.to raise_error(ActiveRecord::RecordInvalid)
-  	end  
+    it "needs a password <= 72 characters" do
+      expect { create :user, :long_password }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 end
