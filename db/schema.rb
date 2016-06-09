4a1d615754c6ea94ecde211923197e625290e911
mphprogrammer <marc.harbatkin.va@gmail.com>
2016-06-09 12:38:58 -0400

    Added single quotes and other odd characters to strings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330160605) do
+ActiveRecord::Schema.define(version: 20160609143717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -399,6 +399,16 @@ ActiveRecord::Schema.define(version: 20160330160605) do
   end
 
   add_index "sec702s", ["state"], name: "index_sec702s_on_state", unique: true, using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "settlements", force: :cascade do |t|
     t.string   "cross",                  null: false
