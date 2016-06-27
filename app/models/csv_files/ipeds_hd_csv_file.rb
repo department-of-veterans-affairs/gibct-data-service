99e82f2bf76db9fae7e68a324717f8617bff58a9
Marc Harbatkin <marc.harbatkin.va@gmail.com>
2016-06-27 11:19:05 -0400

    Added # to ipeds_hd

--- app/models/csv_files/ipeds_hd_csv_file.rb
+++ app/models/csv_files/ipeds_hd_csv_file.rb
@@ -7,8 +7,8 @@ class IpedsHdCsvFile < CsvFile
   SKIP_LINES_BEFORE_HEADER = 0
   SKIP_LINES_AFTER_HEADER = 0
 
-  DISALLOWED_CHARS = /[^\w@\- \.\/]/
-
+  DISALLOWED_CHARS = /[^#&'\w@:\- \.\/\(\)\+]/
+  
   #############################################################################
   ## populate
   ## Reloads the accreditation table with the data in the csv data store
