5308b864381b0d05ce32654d607d6f9b274bb03a
Marc Harbatkin <marc.harbatkin.va@gmail.com>
2016-06-29 13:05:40 -0400

    added ? and = to urls

--- app/models/csv_files/ipeds_hd_csv_file.rb
+++ app/models/csv_files/ipeds_hd_csv_file.rb
@@ -7,7 +7,7 @@ class IpedsHdCsvFile < CsvFile
   SKIP_LINES_BEFORE_HEADER = 0
   SKIP_LINES_AFTER_HEADER = 0
 
-  DISALLOWED_CHARS = /[^#&'\w@:\- \.\/\(\)\+]/
+  DISALLOWED_CHARS = /[^#&'@:=\w\- \.\/\(\)\+\?]/
   
   #############################################################################
   ## populate
