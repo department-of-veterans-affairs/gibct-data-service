67c897b0b7dd6d53b31fb0307be1b34257f20327
mphprogrammer <marc.harbatkin.va@gmail.com>
2017-01-06 10:13:22 -0500

    rubocop and some pendings

--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe VersionsController, type: :controller do
-
+  pending 'will be added later'
 end
--- spec/controllers/weams_controller_spec.rb
+++ spec/controllers/weams_controller_spec.rb
@@ -2,4 +2,5 @@
 require 'rails_helper'
 
 RSpec.describe WeamsController, type: :controller do
+  pending 'will be added later'
 end
