fa5560834687a9838730265558fdf8f411092e82
mphprogrammer <marc.harbatkin.va@gmail.com>
2017-01-16 01:14:15 -0500

    Completed Scorecards

--- spec/factories/scorecards.rb
+++ spec/factories/scorecards.rb
@@ -5,19 +5,17 @@ FactoryGirl.define do
 
     pred_degree_awarded 0
 
-    trait :c150_4_pooled_supp do
+    c150_4_pooled_supp 1
+    c150_l4_pooled_supp 2
+
+    trait :by_c150_4_pooled_supp do
       c150_4_pooled_supp 1
       c150_l4_pooled_supp nil
     end
 
-    trait :c150_l4_pooled_supp do
+    trait :by_c150_l4_pooled_supp do
       c150_4_pooled_supp nil
       c150_l4_pooled_supp 1
-    end
-
-    trait :c150_4_pooled_supp_over_c150_l4_pooled_supp do
-      c150_4_pooled_supp 1
-      c150_l4_pooled_supp 2
     end
   end
 end
--- spec/models/scorecard_spec.rb
+++ spec/models/scorecard_spec.rb
@@ -10,8 +10,21 @@ RSpec.describe Scorecard, type: :model do
   describe 'when validating' do
     subject { build :scorecard }
 
+    let(:by_c150_4_pooled_supp) { create(:scorecard, :by_c150_4_pooled_supp) }
+    let(:by_c150_l4_pooled_supp) { create(:scorecard, :by_c150_l4_pooled_supp) }
+
     it 'has a valid factory' do
       expect(subject).to be_valid
+    end
+
+    it 'will use either the c150_4_pooled_supp or the c150_l4_pooled_supp if only one is present' do
+      expect(by_c150_4_pooled_supp.graduation_rate_all_students).to eq(by_c150_4_pooled_supp.c150_4_pooled_supp)
+      expect(by_c150_l4_pooled_supp.graduation_rate_all_students).to eq(by_c150_l4_pooled_supp.c150_l4_pooled_supp)
+    end
+
+    it 'prefers c150_4_pooled_supp over c150_l4_pooled_supp' do
+      subject.valid?
+      expect(subject.graduation_rate_all_students).to eq(subject.c150_4_pooled_supp)
     end
   end
 end
