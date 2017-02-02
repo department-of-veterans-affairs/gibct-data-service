61573d0cb5f19cb6c4458b6004988538e546ffa5
Ben Damman <benjamin_c_damman@omb.eop.gov>
2017-02-02 18:36:58 -0500

    I <3 Rubocop

--- app/controllers/concerns/session_cleaning.rb
+++ app/controllers/concerns/session_cleaning.rb
@@ -3,6 +3,6 @@ module SessionCleaning
   extend ActiveSupport::Concern
 
   def delete_inactive_sessions!
-    ActiveRecord::SessionStore::Session.delete_all ["created_at < ?", 1.week.ago]
+    ActiveRecord::SessionStore::Session.delete_all ['created_at < ?', 1.week.ago]
   end
 end
