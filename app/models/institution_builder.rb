b9caef890c9d11e2b5ea6a0a69bc60dcedd14484
mphprogrammer <marc.harbatkin.va@gmail.com>
2017-02-05 00:59:46 -0500

    adding Accreditation to InstitutionBuilder

--- app/models/institution_builder.rb
+++ app/models/institution_builder.rb
@@ -98,4 +98,105 @@ module InstitutionBuilder
 
     Institution.connection.update(str)
   end
+
+  # Updates the institution table with data from the accreditations table. There is an explict
+  # ordering of both accreditation type and status for those schools having conflicting types
+  # hybrid < national < regional, and for status: 'show cause' < probation. There is a
+  # requirement to include only those accreditations that are institutional and currently active.
+  def self.update_with_accreditation
+    # Sets all accreditations that are hybrid types first
+    str = 'UPDATE institutions SET '
+    str += 'accreditation_type = accreditations.accreditation_type'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += %(AND LOWER(accreditations.csv_accreditation_type) = 'institutional' )
+    str += "AND LOWER(accreditations.accreditation_type) = 'hybrid'; "
+
+    # Sets all accreditations that are national, overriding conflicts with hybrids
+    str += 'UPDATE institutions SET '
+    str += 'accreditation_type = accreditations.accreditation_type'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += %(AND LOWER(accreditations.csv_accreditation_type) = 'institutional' )
+    str += "AND LOWER(accreditations.accreditation_type) = 'national';"
+
+    # Sets all accreditations that are regional, overriding conflicts with hybrids and nationals
+    str += 'UPDATE institutions SET '
+    str += 'accreditation_type = accreditations.accreditation_type'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += %(AND LOWER(accreditations.csv_accreditation_type) = 'institutional' )
+    str += "AND LOWER(accreditations.accreditation_type) = 'regional'; "
+
+    # Sets status for probationary accreditations, the status being derived from the
+    # higest level of accreditation (hybrid, national, regional).
+    str += 'UPDATE institutions SET '
+    str += 'accreditation_status = accreditations.accreditation_status'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND institutions.accreditation_type = accreditations.accreditation_type '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += "AND LOWER(accreditations.csv_accreditation_type) = 'institutional' "
+    str += "AND LOWER(accreditations.accreditation_status) = 'probation'; "
+
+    # Sets status for show cause accreditations, overwriting probationary statuses,
+    # the status being derived from the higest level of accreditation (hybrid, national, regional).
+    str += 'UPDATE institutions SET '
+    str += 'accreditation_status = accreditations.accreditation_status'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND institutions.accreditation_type = accreditations.accreditation_type '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += "AND LOWER(accreditations.csv_accreditation_type) = 'institutional' "
+    str += "AND LOWER(accreditations.accreditation_status) = 'show cause'; "
+
+    # Sets the caution flag for all accreditations that have a non-null status. Note,
+    # that institutional type accreditations are always, null, probation, or show cause.
+    str += 'UPDATE institutions SET '
+    str += 'caution_flag = TRUE'
+    str += ' FROM accreditations '
+    str += 'WHERE institutions.cross = accreditations.cross '
+    str += 'AND accreditations.cross IS NOT NULL '
+    str += %(AND LOWER(accreditations.periods) LIKE '%current%' )
+    str += 'AND accreditations.accreditation_status IS NOT NULL '
+    str += "AND LOWER(accreditations.csv_accreditation_type) = 'institutional'; "
+
+    # Sets the caution flag reason for all accreditations that have a non-null status.
+    # The innermost subquery retrieves a unique set of statues (it is plausible that
+    # identical statuses may apply to the same school but from different agencies). We
+    # don't want to repeat the same reasons. The outer subquery then concatentates these
+    # unique reasons into a comma-separated string. Lastly the outer UPDATE-CASE appends
+    # these caution flag reasons to any existing caution flag reasons.
+    str += 'UPDATE institutions SET caution_flag_reason = '
+    str += 'CASE WHEN institutions.caution_flag_reason IS NULL THEN '
+    str += '  AGG.ad '
+    str += 'ELSE '
+    str += "  CONCAT(institutions.caution_flag_reason, ', ', AGG.ad) "
+    str += 'END '
+    str += 'FROM ( '
+    str += "  SELECT A.cross, string_agg(A.AST, ', ') AS ad "
+    str += '    FROM ('
+    str += "      SELECT a.cross, 'Accreditation (' || a.accreditation_status || ')' as AST"
+    str += '      FROM accreditations a '
+    str += '      WHERE '
+    str += '        a.cross IS NOT NULL AND '
+    str += '        a.accreditation_status IS NOT NULL AND '
+    str += "        LOWER(a.periods) LIKE '%current%' AND "
+    str += "        LOWER(a.csv_accreditation_type) = 'institutional' "
+    str += '      GROUP BY a.cross, a.accreditation_status '
+    str += '    ) A '
+    str += '  GROUP BY A.cross '
+    str += ') AGG '
+    str += 'WHERE institutions.cross = AGG.cross; '
+
+    Institution.connection.update(str)
+  end
 end
