247b765edf901dc6b01cff8b88a4205d395cf1d3
mphprogrammer <marc.harbatkin.va@gmail.com>
2017-03-13 12:36:40 -0400

    Removed outcome defaults

--- db/migrate/20170124200527_create_outcomes.rb
+++ db/migrate/20170124200527_create_outcomes.rb
@@ -4,17 +4,17 @@ class CreateOutcomes < ActiveRecord::Migration
       # Used in the building of DataCsv
       t.string :facility_code, null: false
 
-      t.float :retention_rate_veteran_ba, default: 0.00
-      t.float :retention_rate_veteran_otb, default: 0.00
-      t.float :persistance_rate_veteran_ba, default: 0.00
-      t.float :persistance_rate_veteran_otb, default: 0.00
-      t.float :graduation_rate_veteran, default: 0.00
-      t.float :transfer_out_rate_veteran, default: 0.00
+      t.float :retention_rate_veteran_ba
+      t.float :retention_rate_veteran_otb
+      t.float :persistance_rate_veteran_ba
+      t.float :persistance_rate_veteran_otb
+      t.float :graduation_rate_veteran
+      t.float :transfer_out_rate_veteran
 
       # Not used in building DataCsv, but used in exporting source csv
       t.string :institution
       t.string :school_level_va
-      
+
       t.timestamps null: false
 
       t.index :facility_code, unique: true
