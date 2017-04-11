15046b18e2774b48a06eafbeebd76819c075b0a4
Kam Karshenas <kam@saneshark.com>
2017-04-11 14:52:57 -0400

    version meta flag for preview had wrong logic (#248)

--- app/controllers/v0/api_controller.rb
+++ app/controllers/v0/api_controller.rb
@@ -20,7 +20,7 @@ module V0
       @version = {
         number: version.number,
         created_at: version.created_at,
-        preview: version.preview?
+        preview: !version.production?
       }
     end
 
