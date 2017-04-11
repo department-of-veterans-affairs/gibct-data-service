d118adc151b9440b2102ed04d9377647d8eecc2c
Kam Karshenas <kam@saneshark.com>
2017-04-11 01:01:21 -0400

    fixing version link logic

--- app/models/version.rb
+++ app/models/version.rb
@@ -43,12 +43,16 @@ class Version < ActiveRecord::Base
   end
 
   def gibct_link
+    version_info = production? ? '' : "?version=#{uuid}"
+    return "#{ENV['GIBCT_HOST']}#{version_info}" if ENV['GIBCT_HOST']
+
     base_link = ENV['LINK_HOST'].gsub(/(^\w+:|^)\/\//, '').split('.')
-    if base_link.size == 3 # dev, staging, or production
+    base_link = if base_link.size == 3 # dev, staging, or production
       "https://#{base_link[0].gsub(/(-api|api)/, '')}.vets.gov"
     else # localhost (where 3000 gids, 3001 vets-api, and 3002 vets-website)
-      "localhost:3002"
+      "http://localhost:3002"
     end
+    "#{base_link}/gi-bill-comparison-tool#{version_info}"
   end
 
   def self.production_version
