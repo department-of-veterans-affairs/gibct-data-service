ccd57b4715d679069b42c7500dd09bbaf84cc5c6
Patrick Vinograd <patrick@adhocteam.us>
2017-04-12 09:31:06 -0700

    Revert "Allow navigate to form without csv_type"
    This reverts commit b757b1bde32a8da3e9d69cfaa29164dee6004386.

--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -57,11 +57,17 @@ RSpec.describe UploadsController, type: :controller do
     end
 
     context 'specifying no csv_type' do
-      it 'generates a new Upload and renders form' do
-        get(:new)
+      it 'redirects to the dashboard' do
+        expect(
+          get(:new)
+        ).to redirect_to('/dashboards')
+      end
 
-        expect(assigns(:upload).skip_lines).to eq(0)
-        expect(response).to render_template('new')
+      it 'formats an error message in the flash' do
+        get :new
+
+        expect(flash[:alert]).to be_present
+        expect(flash[:alert].first).to match(/is not a valid CSV data source/)
       end
     end
   end
