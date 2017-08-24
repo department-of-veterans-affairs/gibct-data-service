b15258b71fab28daef825a8e0beb6e7bcbe9ef00
mphprogrammer <marc.harbatkin@va.gov>
2017-08-24 10:53:14 -0400

    Reset changes caused by emergency constants push to resolve conflict in seeds

--- db/seeds/02_data.rb
+++ db/seeds/02_data.rb
@@ -23,6 +23,34 @@ def seed_table(klass, user, options = {})
   puts 'Done!'
 end
 
+puts 'Creating sample constants'
+constants = {
+  'TFCAP' => 21970.46,
+  'AVGBAH' => 1611,
+  'BSCAP' => 1000,
+  'BSOJTMONTH' => 83,
+  'FLTTFCAP' => 12554.54,
+  'CORRESPONDTFCAP' => 10671.35,
+  'MGIB3YRRATE' => 1857,
+  'MGIB2YRRATE' => 1509,
+  'SRRATE' => 369,
+  'DEARATE' => 1024,
+  'DEARATEOJT' => 747,
+  'VRE0DEPRATE' => 605.44,
+  'VRE1DEPRATE' => 751.00,
+  'VRE2DEPRATE' => 885.00,
+  'VREINCRATE' => 64.50,
+  'VRE0DEPRATEOJT' => 529.36,
+  'VRE1DEPRATEOJT' => 640.15,
+  'VRE2DEPRATEOJT' => 737.77,
+  'VREINCRATEOJT' => 47.99,
+  'AVERETENTIONRATE' => 67.7,
+  'AVEGRADRATE' => 42.3,
+  'AVESALARY' => 33_400,
+  'AVEREPAYMENTRATE' => 67.9
+}.map { |k,v| {name: k, float_value: v} }
+CalculatorConstant.create(constants)
+
 user = User.first
 
 puts 'Deleting old versions'
