8bd242caa01ed16fc71ac413257febf709e075a7
mphprogrammer <marc.harbatkin@va.gov>
2017-08-24 12:04:25 -0400

    Manually synced master to raw for seeds

--- db/seeds/03_data.rb
+++ db/seeds/03_data.rb
@@ -3,7 +3,7 @@ def seed_table(klass, user, options = {})
   csv_type = klass.name
   csv_path = 'sample_csvs'
 
-  print "Loading #{klass.name} from #{csv_path}/#{csv_name} ... "
+  puts "Loading #{klass.name} from #{csv_path}/#{csv_name} ... "
 
   uf = ActionDispatch::Http::UploadedFile.new(
     tempfile: File.new(Rails.root.join(csv_path, csv_name)),
@@ -15,41 +15,13 @@ def seed_table(klass, user, options = {})
   klass.load("#{csv_path}/#{csv_name}", options)
   upload.update(ok: true)
 
-  print "Loading #{klass.name} storage from #{csv_path}/#{csv_name} ... "
+  puts "Loading #{klass.name} storage from #{csv_path}/#{csv_name} ... "
   uf.rewind
 
   Storage.create(upload_file: uf, csv_type: csv_type, comment: 'Seeding', user: user)
 
   puts 'Done!'
 end
-
-puts 'Creating sample constants'
-constants = {
-  'TFCAP' => 21970.46,
-  'AVGBAH' => 1611,
-  'BSCAP' => 1000,
-  'BSOJTMONTH' => 83,
-  'FLTTFCAP' => 12554.54,
-  'CORRESPONDTFCAP' => 10671.35,
-  'MGIB3YRRATE' => 1857,
-  'MGIB2YRRATE' => 1509,
-  'SRRATE' => 369,
-  'DEARATE' => 1024,
-  'DEARATEOJT' => 747,
-  'VRE0DEPRATE' => 605.44,
-  'VRE1DEPRATE' => 751.00,
-  'VRE2DEPRATE' => 885.00,
-  'VREINCRATE' => 64.50,
-  'VRE0DEPRATEOJT' => 529.36,
-  'VRE1DEPRATEOJT' => 640.15,
-  'VRE2DEPRATEOJT' => 737.77,
-  'VREINCRATEOJT' => 47.99,
-  'AVERETENTIONRATE' => 67.7,
-  'AVEGRADRATE' => 42.3,
-  'AVESALARY' => 33_400,
-  'AVEREPAYMENTRATE' => 67.9
-}.map { |k,v| {name: k, float_value: v} }
-CalculatorConstant.create(constants)
 
 user = User.first
 
