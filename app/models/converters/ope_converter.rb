83ce09dc59e8f4d805fe671d76ec91029cfc2f95
MKommidi <maneshkommidi@gmail.com>
2018-04-02 15:11:24 -0400

    Fixed the OPE_ID quotes getting stripped issue

--- app/models/converters/ope_converter.rb
+++ app/models/converters/ope_converter.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 # Right justifies ope to 8 characters using 0s.
-class OpeConverter < BaseConverter
+class OpeConverter
   def self.convert(value)
-    value = super(value.to_s)
+    value = value.to_s
+    value = value.match(/\A(NONE|\.)\z/i).nil? ? value : nil
     value.blank? ? nil : value.rjust(8, '0')
   end
 end
