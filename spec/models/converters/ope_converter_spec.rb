1658e6ffae321a2f124817a33526ae8875ce4050
MKommidi <maneshkommidi@gmail.com>
2018-04-03 14:07:04 -0400

    Addressed PR comments

--- spec/models/converters/ope_converter_spec.rb
+++ spec/models/converters/ope_converter_spec.rb
@@ -4,6 +4,15 @@ require 'rails_helper'
 RSpec.describe OpeConverter do
   subject { described_class }
 
+  it 'converts a string composed of a word NONE to nil' do
+    expect(subject.convert('NONE')).to be_nil
+  end
+
+  it 'allows double quotation marks for OPE column strings' do
+    str = %("this string has double quotes")
+    expect(subject.convert(str)).to eq('"this string has double quotes"')
+  end
+
   it 'right justifies with leading 0s to 8 digits in length' do
     expect(subject.convert('1')).to eq('00000001')
   end
