bc3d25528ba58eb5edbc96932fe50475dc666d80
MKommidi <maneshkommidi@gmail.com>
2018-04-12 09:15:22 -0400

    Fixed the IEIPDS Issue

--- spec/models/converters/cross_converter_spec.rb
+++ spec/models/converters/cross_converter_spec.rb
@@ -4,16 +4,12 @@ require 'rails_helper'
 RSpec.describe CrossConverter do
   subject { described_class }
 
-  it 'right justifies with leading 0s to 8 digits in length' do
-    expect(subject.convert('1')).to eq('00000001')
-  end
-
   it 'returns nil if value is blank' do
     expect(subject.convert(nil)).to be_nil
     expect(subject.convert('   ')).to be_nil
   end
 
   it 'converts any letters to upcase' do
-    expect(subject.convert('123Xx4')).to eq('00123XX4')
+    expect(subject.convert('123Xx4')).to eq('123XX4')
   end
 end
