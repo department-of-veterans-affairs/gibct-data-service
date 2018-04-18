dc9b36d453c1d48432b5a69db6c0ec36f3180543
MKommidi <maneshkommidi@gmail.com>
2018-04-18 09:45:22 -0400

    Fixed the test suite

--- app/models/converters/ope_converter.rb
+++ app/models/converters/ope_converter.rb
@@ -4,7 +4,7 @@
 class OpeConverter
   def self.convert(value)
     value = value.to_s
-    value = value.match(/\A(NONE|\.)\z/i).nil? ? value : nil
+    value = value.match(/\A(NONE|null|\.)\z/i).nil? ? value : nil
     value.blank? ? nil : value.rjust(8, '0')
   end
 end
