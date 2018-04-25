eeaa03ff37f134d4ddcda978c5a493e921283998
Adithya Pentela <adithya3678@gmail.com>
2018-04-24 23:54:05 -0400

    Removed timeoutable and using the action in application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   has_many :storages, inverse_of: :user
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :omniauthable and
+  # :confirmable, :lockable, :timeoutable, :omniauthable and
   # :registerable
-  devise :database_authenticatable, :timeoutable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.from_saml_callback(response_attrs)
