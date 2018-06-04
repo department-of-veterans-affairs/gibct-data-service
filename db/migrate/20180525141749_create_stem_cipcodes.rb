c9e356a67c955ee5ea36a2001252c0fe6b3b374a
James Kassemi <james.kassemi@adhocteam.us>
2018-06-04 04:22:04 -0600

    spacing

--- db/migrate/20180525141749_create_stem_cipcodes.rb
+++ db/migrate/20180525141749_create_stem_cipcodes.rb
@@ -1,7 +1,7 @@
 class CreateStemCipcodes < ActiveRecord::Migration
   def change
     create_table :stem_cip_codes do |t|
-    	t.integer :two_digit_series
+      t.integer :two_digit_series
       t.string :twentyten_cip_code, index: true
       t.string :cip_code_title
 
