ac250898ed4f1b1f3a1e35643bb12e651823e223
Kam Karshenas <kam@adhocteam.us>
2018-06-08 14:55:26 -0700

    move index into its own migration

--- db/migrate/20180525130652_change_defaults_add_index_for_stem_offered_on_institutions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDefaultsAddIndexForStemOfferedOnInstitutions < ActiveRecord::Migration
-  def change
-    change_column_default :institutions, :stem_offered, false
-    add_index :institutions, :stem_offered
-  end
-end
