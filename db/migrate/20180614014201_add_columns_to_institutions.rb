b3e14d1f389d5bea4e6ee49250810ff00819c0dc
Kam Karshenas <kam@saneshark.com>
2018-06-22 08:59:50 -0700

    Migrations for WEAMS and Institutions (#340)
    * adding migrations and the new weams file

---
+++ db/migrate/20180614014201_add_columns_to_institutions.rb
@@ -0,0 +1,9 @@
+class AddColumnsToInstitutions < ActiveRecord::Migration
+  def change
+    # Note this is going to result in null values, three-state-boolean problem
+    add_column :institutions, :priority_enrollment, :boolean
+    add_column :institutions, :online_only, :boolean
+    add_column :institutions, :independent_study, :boolean
+    add_column :institutions, :distance_learning, :boolean
+  end
+end
