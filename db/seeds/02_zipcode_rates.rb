898a34e29c8af27ecc43f45401987ce35183ac41
Kam Karshenas <kam@adhocteam.us>
2018-06-22 00:20:44 -0700

    minor fixes to address a bug in rails version, and more rubocop

--- db/seeds/02_zipcode_rates.rb
+++ db/seeds/02_zipcode_rates.rb
@@ -1,5 +1,3 @@
-puts 'Deleting old Zipcode Rates'
-ZipcodeRate.delete_all
-
+puts 'Deleting old Zipcode Rates' # klass.load will delete_all
 puts 'Building Zipcode Rates'
 SeedUtils.seed_table(ZipcodeRate, Rails.root.join('sample_csvs', 'zipcode_rate.csv'))
--- lib/seed_utils.rb
+++ lib/seed_utils.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 module SeedUtils
-  extend self
+  module_function
 
   def seed_table_with_upload(klass, user, options = {})
-    csv_name = "#{klass.name.underscore}.csv"
     csv_type = klass.name
+    csv_name = "#{csv_type.underscore}.csv"
     csv_path = 'sample_csvs'
 
     puts "Loading #{klass.name} from #{csv_path}/#{csv_name} ... "
 
     uf = ActionDispatch::Http::UploadedFile.new(
       tempfile: File.new(Rails.root.join(csv_path, csv_name)),
-      filename: csv_name,
-      type: 'text/csv'
+      # dup required until upgrade: https://github.com/rails/rails/commit/bfbbb1207930e7ebe56d4a99abd53b2aa66e0b6e
+      filename: csv_name.dup,
+      content_type: 'text/csv'
     )
 
     upload = Upload.create(upload_file: uf, csv_type: csv_type, comment: 'Seeding', user: user)
