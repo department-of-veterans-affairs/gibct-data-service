ad5256e9eb19108401e663cca7b4a3a52046c20e
Lihan Li <frankieteardrop@gmail.com>
2018-10-25 11:47:07 -0700

    add new indexes for online only and distance learning

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019181601) do
+ActiveRecord::Schema.define(version: 20181025184547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -287,9 +287,11 @@ ActiveRecord::Schema.define(version: 20181019181601) do
   add_index "institutions", ["address_3"], name: "index_institutions_on_address_3", using: :btree
   add_index "institutions", ["city"], name: "index_institutions_on_city", using: :btree
   add_index "institutions", ["cross"], name: "index_institutions_on_cross", using: :btree
+  add_index "institutions", ["distance_learning"], name: "index_institutions_on_distance_learning", using: :btree
   add_index "institutions", ["facility_code"], name: "index_institutions_on_facility_code", using: :btree
   add_index "institutions", ["institution"], name: "index_institutions_on_institution", using: :btree
   add_index "institutions", ["institution_type_name"], name: "index_institutions_on_institution_type_name", using: :btree
+  add_index "institutions", ["online_only"], name: "index_institutions_on_online_only", using: :btree
   add_index "institutions", ["ope6"], name: "index_institutions_on_ope6", using: :btree
   add_index "institutions", ["state"], name: "index_institutions_on_state", using: :btree
   add_index "institutions", ["stem_offered"], name: "index_institutions_on_stem_offered", using: :btree
