a09e1a54ca40f58449d214f6ea9ac53098fbade7
Johnny Holton <johnny@holton.co>
2019-02-26 15:08:05 -0500

    add config for links inside emails

--- config/initializers/hostname.rb
+++ config/initializers/hostname.rb
@@ -1,2 +1,6 @@
 protocol, host = Figaro.env.link_host!.split('://')
 Rails.application.routes.default_url_options = { protocol: protocol, host: host }
+
+protocol, home_url = Figaro.env.gibct_url!.split('://')
+host = home_url.split('/').first
+Rails.application.config.action_mailer.default_url_options = { protocol: protocol, host: host }
