af9b4608e264fb7bc3486493f1cddd2e1eb12c9d
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-10 15:11:49 -0400

    Update zipcode rate model

--- app/models/zipcode_rate.rb
+++ app/models/zipcode_rate.rb
@@ -8,6 +8,9 @@ class ZipcodeRate < ActiveRecord::Base
     'mha_code' => { column: :mha_code, converter: BaseConverter },
     'mha_name' => { column: :mha_name, converter: BaseConverter },
     'mha_rate' => { column: :mha_rate, converter: NumberConverter },
+    'dod_mha_rate' => { column: :dod_mha_rate, converter: NumberConverter },
     'mha_rate_grandfathered' => { column: :mha_rate_grandfathered, converter: NumberConverter }
   }.freeze
+
+  scope :version, ->(n) { where(version: n) }
 end
