62c108d2be4b6350952227016e33544620d91498
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-11 14:34:55 -0400

    Added unused grandfathered rate field back in

--- app/models/zipcode_rate.rb
+++ app/models/zipcode_rate.rb
@@ -8,7 +8,8 @@ class ZipcodeRate < ActiveRecord::Base
     'mha_code' => { column: :mha_code, converter: BaseConverter },
     'mha_name' => { column: :mha_name, converter: BaseConverter },
     'mha_rate' => { column: :mha_rate, converter: NumberConverter },
-    'dod_mha_rate' => { column: :dod_mha_rate, converter: NumberConverter },
+    'mha_rate_grandfathered' => { column: :mha_rate_grandfathered, converter: NumberConverter },
+    'dod_mha_rate' => { column: :dod_mha_rate, converter: NumberConverter }
   }.freeze
 
   validates :version, presence: true
