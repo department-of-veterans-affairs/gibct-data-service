4198d18ed83a2ff829664325617cb3654709d361
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-11 16:35:39 -0400

    Add zipcode_rate indexes to migration

--- db/migrate/20190610114000_add_columns_to_zipcode_rates.rb
+++ db/migrate/20190610114000_add_columns_to_zipcode_rates.rb
@@ -2,5 +2,8 @@ class AddColumnsToZipcodeRates < ActiveRecord::Migration
   def change
     add_column :zipcode_rates, :dod_mha_rate, :float
     add_column :zipcode_rates, :version, :integer
+
+    add_index :zipcode_rates, :zip_code, name: "index_zipcode_rates_on_zip_code", using: :btree
+    add_index :zipcode_rates, :version, name: "index_zipcode_rates_on_version", using: :btree
   end
 end
