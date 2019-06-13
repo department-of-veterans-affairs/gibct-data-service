1bbcc21293f37a1f2c0cc245b6b023c2aeadfc8d
Matt Roth <mattcroth@gmail.com>
2019-06-13 14:49:39 -0400

    Update db/migrate/20190607014201_add_column_approved_to_institutions.rb
    Co-Authored-By: Johnny Holton <johnny@holton.co>

--- db/migrate/20190607014201_add_column_approved_to_institutions.rb
+++ db/migrate/20190607014201_add_column_approved_to_institutions.rb
@@ -1,6 +1,6 @@
 # Define `table_name` in a custom named class to make sure that you run on the
 # same table you had during the creation of the migration.
-# In future if you override the `Product` class and change the `table_name`,
+# In future if you override the `Institution` class and change the `table_name`,
 # it won't break the migration or cause serious data corruption.
 class InstitutionModel < ActiveRecord::Base
   self.table_name = :institutions
