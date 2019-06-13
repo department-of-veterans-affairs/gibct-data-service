b548bb4a3b5f9ffb788b1f869a571d268368dd34
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-06-13 10:44:37 -0400

    changed added column

--- db/migrate/20190611018894_add_column_vet_tec_provider_to_institutions.rb
+++ db/migrate/20190611018894_add_column_vet_tec_provider_to_institutions.rb
@@ -1,6 +1,5 @@
 class AddColumnVetTecProviderToInstitutions < ActiveRecord::Migration
   def change
-    # Note this is going to result in null values, three-state-boolean problem
     add_column :institutions, :vet_tec_provider, :boolean, default: false, null: false
   end
 end
