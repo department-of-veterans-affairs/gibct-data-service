e0d5e97ccf0545cd1fc40baa63fce52d367dd400
Matt Roth <roth_matthew@bah.com>
2019-06-13 14:27:31 -0400

    best practices

--- db/migrate/20190607014201_add_column_approved_to_institutions.rb
+++ db/migrate/20190607014201_add_column_approved_to_institutions.rb
@@ -1,9 +1,17 @@
+# Define `table_name` in a custom named class to make sure that you run on the
+# same table you had during the creation of the migration.
+# In future if you override the `Product` class and change the `table_name`,
+# it won't break the migration or cause serious data corruption.
+class InstitutionModel < ActiveRecord::Base
+  self.table_name = :institutions
+end
+
 class AddColumnApprovedToInstitutions < ActiveRecord::Migration
   def change
     add_column :institutions, :approved, :boolean, null: false, default: false
     reversible do |dir|
       dir.up do
-        Institution.update_all(approved: true)
+        InstitutionModel.update_all(approved: true)
       end
     end
   end
