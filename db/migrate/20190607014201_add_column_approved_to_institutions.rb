1b721583cc123ba2d35118bd9d593f95520affa3
Matt Roth <roth_matthew@bah.com>
2019-06-14 13:26:43 -0400

    add rake script

--- db/migrate/20190607014201_add_column_approved_to_institutions.rb
+++ db/migrate/20190607014201_add_column_approved_to_institutions.rb
@@ -1,18 +1,5 @@
-# Define `table_name` in a custom named class to make sure that you run on the
-# same table you had during the creation of the migration.
-# In future if you override the `Institution` class and change the `table_name`,
-# it won't break the migration or cause serious data corruption.
-class InstitutionModel < ActiveRecord::Base
-  self.table_name = :institutions
-end
-
 class AddColumnApprovedToInstitutions < ActiveRecord::Migration
   def change
     add_column :institutions, :approved, :boolean, null: false, default: false
-    reversible do |dir|
-      dir.up do
-        InstitutionModel.update_all(approved: true)
-      end
-    end
   end
 end
