587a6ac23f60eff6e759ed77dd2e875325732a5e
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-06-21 15:15:41 -0400

    added stem indicator column to weams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190613018894) do
+ActiveRecord::Schema.define(version: 20190622019025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1297,6 +1297,7 @@ ActiveRecord::Schema.define(version: 20190613018894) do
     t.string   "physical_zip"
     t.string   "physical_country"
     t.integer  "dod_bah"
+    t.boolean  "stem_indicator"
   end
 
   add_index "weams", ["facility_code"], name: "index_weams_on_facility_code", unique: true, using: :btree
