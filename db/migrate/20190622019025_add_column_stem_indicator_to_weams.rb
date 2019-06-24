bc7c382e42f652346fcb938e5581f288c77abde4
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-06-24 08:21:42 -0400

    Erased comment

--- db/migrate/20190622019025_add_column_stem_indicator_to_weams.rb
+++ db/migrate/20190622019025_add_column_stem_indicator_to_weams.rb
@@ -1,6 +1,5 @@
 class AddColumnStemIndicatorToWeams < ActiveRecord::Migration
     def change
-      # Note this is going to result in null values, three-state-boolean problem
       add_column :weams, :stem_indicator, :boolean
     end
   end
