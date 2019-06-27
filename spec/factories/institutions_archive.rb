e46bf5996c13840178c25322d090ca0a06680161
Matt Roth <roth_matthew@bah.com>
2019-06-27 14:16:58 -0400

    spec works now, silly inheritance

--- spec/factories/institutions_archive.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :institutions_archive do
-  end
-end
--- spec/models/institutions_archive_spec.rb
+++ spec/models/institutions_archive_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe InstitutionsArchive, type: :model do
   end
 
   describe 'archive' do
-    it 'archives version 1' do
+    it 'archives old version without preview versions greater than current production' do
 
       # version 1
       create_production_version    
@@ -21,23 +21,108 @@ RSpec.describe InstitutionsArchive, type: :model do
       create_production_version
       create :institution, version: current_production_number
 
-      puts Institution.all
-      puts "delete"
-      InstitutionsArchive.delete_all
-      puts Institution.all
-
       expect(Institution.count).to eq(2)
       expect(InstitutionsArchive.count).to eq(0)
 
-      # InstitutionsArchive.archive(Version.current_production)
+      InstitutionsArchive.archive(Version.current_production)
       
-      # expect(Institution.count).to eq(1)
-      # expect(Institution.where("version = ?", current_production_number).size).to eq(1)
+      expect(Institution.count).to eq(1)
+      expect(Institution.where("version >= ?", current_production_number).size).to eq(1)
 
-      # expect(InstitutionsArchive.count).to eq(1)
-      # expect(InstitutionsArchive.where("version < ?", current_production_number).size).to eq(1)
+      expect(InstitutionsArchive.count).to eq(1)
+      expect(InstitutionsArchive.where("version < ?", current_production_number).size).to eq(1)
     end
 
+    it 'archives old version with preview versions greater than current production' do
+
+      # version 1
+      create_production_version    
+      create :institution, version: current_production_number
+
+      # version 2
+      create_production_version
+      create :institution, version: current_production_number
+
+      # preview version 3
+      create :version, :preview
+      create :institution, version: current_preview_number
+
+      expect(Institution.count).to eq(3)
+      expect(InstitutionsArchive.count).to eq(0)
+
+      InstitutionsArchive.archive(Version.current_production)
+      
+      expect(Institution.count).to eq(2)
+      expect(Institution.where("version >= ?", current_production_number).size).to eq(2)
+
+      expect(InstitutionsArchive.count).to eq(1)
+      expect(InstitutionsArchive.where("version < ?", current_production_number).size).to eq(1)
+    end
+
+
+    it 'archives multiple old versions without preview versions greater than current production' do
+
+      # version 1
+      create_production_version    
+      create :institution, version: current_production_number
+
+      # version 2
+      create_production_version
+      create :institution, version: current_production_number
+     
+      # version 3 
+      create_production_version
+      create :institution, version: current_production_number
+
+      # version 4
+      create_production_version
+      create :institution, version: current_production_number
+
+      expect(Institution.count).to eq(4)
+      expect(InstitutionsArchive.count).to eq(0)
+
+      InstitutionsArchive.archive(Version.current_production)
+      
+      expect(Institution.count).to eq(1)
+      expect(Institution.where("version >= ?", current_production_number).size).to eq(1)
+
+      expect(InstitutionsArchive.count).to eq(3)
+      expect(InstitutionsArchive.where("version < ?", current_production_number).size).to eq(3)
+    end
+
+    it 'archives multiple old versions with preview versions greater than current production' do
+
+      # version 1
+      create_production_version    
+      create :institution, version: current_production_number
+
+      # version 2
+      create_production_version
+      create :institution, version: current_production_number
+     
+      # version 3 
+      create_production_version
+      create :institution, version: current_production_number
+
+      # version 4
+      create_production_version
+      create :institution, version: current_production_number
+
+      # preview version 5
+      create :version, :preview
+      create :institution, version: current_preview_number
+
+      expect(Institution.count).to eq(5)
+      expect(InstitutionsArchive.count).to eq(0)
+
+      InstitutionsArchive.archive(Version.current_production)
+      
+      expect(Institution.count).to eq(2)
+      expect(Institution.where("version >= ?", current_production_number).size).to eq(2)
+
+      expect(InstitutionsArchive.count).to eq(3)
+      expect(InstitutionsArchive.where("version < ?", current_production_number).size).to eq(3)
+    end
   end
 
   # private methods
