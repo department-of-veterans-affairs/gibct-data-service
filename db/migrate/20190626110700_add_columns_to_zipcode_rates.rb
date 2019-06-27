1a4f177d8f12c87009d8454cc7f723c0b35f860c
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 11:48:08 -0400

    Cleaner zipcode_rate index creation

--- db/migrate/20190626110700_add_columns_to_zipcode_rates.rb
+++ db/migrate/20190626110700_add_columns_to_zipcode_rates.rb
@@ -2,7 +2,6 @@ class AddColumnsToZipcodeRates < ActiveRecord::Migration
   def change
     add_column :zipcode_rates, :version, :integer
 
-    add_index :zipcode_rates, :zip_code, name: "index_zipcode_rates_on_zip_code", using: :btree
-    add_index :zipcode_rates, :version, name: "index_zipcode_rates_on_version", using: :btree
+    add_index :zipcode_rates, :[zip_code, :version] , using: :btree
   end
 end
