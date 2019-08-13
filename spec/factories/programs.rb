235a24b70db25bd18f8e0e4fb41b0dfb873355c7
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-08-13 11:32:54 -0400

    Added programs.csv to GIDS

---
+++ spec/factories/programs.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :programs do
+    facility_code { generate :facility_code }
+    institution_name { 'NORTHEASTERN ILLINOIS UNIVERSITY' }
+    program_type 'NCD'
+    description 'COMPUTER SCIENCE'
+    full_time_undergraduate '360'
+    graduate '234'
+    full_time_modifier 'S'
+    length '1000'
+
+    initialize_with do
+      new(facility_code: facility_code, institution_name: institution_name,
+        program_type: program_type, description: description,
+        full_time_undergraduate: full_time_undergraduate, graduate: graduate,
+        full_time_modifier: full_time_modifier, length: length)
+    end
+  end
+end
