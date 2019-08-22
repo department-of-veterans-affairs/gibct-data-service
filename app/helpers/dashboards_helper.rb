6327cbb8f0ce22ae5eacf3c9d7edb6f902dc1b22
Matt Roth <roth_matthew@bah.com>
2019-08-22 15:41:04 -0400

    Update app/helpers/dashboards_helper.rb
    Co-Authored-By: Johnny Holton <johnny@oddball.io>

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -4,7 +4,7 @@ module DashboardsHelper
   def latest_upload_class(upload)
     return '' if upload.ok?
     return 'danger' if CSV_TYPES_REQUIRED_TABLE_NAMES.include?(upload.csv_type)
-    return 'warning' unless CSV_TYPES_REQUIRED_TABLE_NAMES.include?(upload.csv_type)
+    return 'warning'
   end
 
   def latest_upload_title(upload)
