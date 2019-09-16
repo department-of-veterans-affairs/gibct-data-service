554b2db1beb81f3cbbd7c1690935d66e43fbe55e
hughes-dustin <hughes_dustin@bah.com>
2019-09-16 08:39:44 -0400

    fix model and remove redundant code

--- app/serializers/program_serializer.rb
+++ app/serializers/program_serializer.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ProgramSerializer < ActiveModel::Serializer
-  attribute :facility_code
-  attribute :institution_name
   attribute :program_type
   attribute :description
   attribute :full_time_undergraduate
