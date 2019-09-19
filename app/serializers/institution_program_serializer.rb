80610073da24e4e024f413dc9eaca38bc635741c
Devin McCurdy <mccurdy_devin@bah.com>
2019-09-19 15:30:27 -0400

    Update serializer

--- app/serializers/institution_program_serializer.rb
+++ app/serializers/institution_program_serializer.rb
@@ -7,4 +7,14 @@ class InstitutionProgramSerializer < ActiveModel::Serializer
   attribute :graduate
   attribute :full_time_modifier
   attribute :length
+  attribute :school_locale
+  attribute :provider_website
+  attribute :provider_email_address
+  attribute :phone_area_code
+  attribute :phone_number
+  attribute :student_vet_group
+  attribute :student_vet_group_website
+  attribute :vet_success_name
+  attribute :tuition_amount
+  attribute :program_length
 end
