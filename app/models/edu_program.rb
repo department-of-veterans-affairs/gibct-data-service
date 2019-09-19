9579669cef32d00fd64122d9f413bcb98486181e
Devin McCurdy <mccurdy_devin@bah.com>
2019-09-19 09:25:08 -0400

    Added placeholder model

---
+++ app/models/edu_program.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Program < ActiveRecord::Base
+  include CsvHelper
+
+  CSV_CONVERTER_INFO = {
+    'facility code' => { column: :facility_code, converter: FacilityCodeConverter },
+    'institution name' => { column: :institution_name, converter: InstitutionConverter },
+    'program type' => { column: :program_type, converter: BaseConverter },
+    'description' => { column: :description, converter: BaseConverter },
+    'full time undergraduate' => { column: :full_time_undergraduate, converter: BaseConverter },
+    'graduate' => { column: :graduate, converter: BaseConverter },
+    'full time modifier' => { column: :full_time_modifier, converter: BaseConverter },
+    'length' => { column: :length, converter: BaseConverter }
+  }.freeze
+
+  validates :facility_code, presence: true
+end
