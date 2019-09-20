a2bd7914324a5abd41b808d604806d43c65f1f8d
Devin McCurdy <mccurdy_devin@bah.com>
2019-09-20 09:28:38 -0400

    Migration cleanup

--- db/migrate/20190916163700_add_institution_programs_constraint.rb
+++ db/migrate/20190916163700_add_institution_programs_constraint.rb
@@ -1,19 +1,16 @@
 class AddInstitutionProgramsConstraint < ActiveRecord::Migration
   disable_ddl_transaction!
+  safety_assured {
+    def up
+        execute <<-SQL
+          ALTER TABLE institution_programs ADD CONSTRAINT u_constraint UNIQUE (facility_code, description);
+        SQL
+    end
 
-  def up
-    safety_assured {
-      execute <<-SQL
-        ALTER TABLE institution_programs ADD CONSTRAINT u_constraint UNIQUE (facility_code, description);
-      SQL
-    }
-  end
-
-  def down
-    safety_assured {
-      execute <<-SQL
-        ALTER TABLE institution_programs DROP CONSTRAINT u_constraint;
-      SQL
-    }
-  end
+    def down
+        execute <<-SQL
+          ALTER TABLE institution_programs DROP CONSTRAINT u_constraint;
+        SQL
+    end
+  }
 end
