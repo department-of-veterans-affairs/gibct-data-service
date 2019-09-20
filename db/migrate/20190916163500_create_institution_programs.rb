953bf67ca673ffe40d1de91a084782a466434a9f
Devin McCurdy <mccurdy_devin@bah.com>
2019-09-20 13:31:01 -0400

    Updated institution_programs fields

--- db/migrate/20190916163500_create_institution_programs.rb
+++ db/migrate/20190916163500_create_institution_programs.rb
@@ -1,14 +1,13 @@
 class CreateInstitutionPrograms < ActiveRecord::Migration
   def change
     create_table :institution_programs do |t|
-      t.string :facility_code, null: false, :limit => 8
-      t.string :institution_name, null: false, :limit => 80
-      t.string :program_type, null: false
-      t.string :description, :limit => 40
-      t.string :full_time_undergraduate, :limit => 15
-      t.string :graduate, :limit => 15
-      t.string :full_time_modifier, :limit => 1
-      t.string :length, :limit => 7
+      t.string :facility_code, null: false
+      t.string :program_type
+      t.string :description, null: false
+      t.string :full_time_undergraduate
+      t.string :graduate
+      t.string :full_time_modifier
+      t.string :length_in_hours
       t.integer :version
 
       t.index [:facility_code, :description], unique: true
