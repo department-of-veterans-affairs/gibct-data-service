f8286f2fb1bbc8376ae542151a6fc4bbb0bdf34d
Devin McCurdy <mccurdy_devin@bah.com>
2019-09-23 16:24:37 -0400

    Cleanup

--- db/migrate/20190923145100_update_institution_programs_index.rb
+++ db/migrate/20190923145100_update_institution_programs_index.rb
@@ -4,7 +4,7 @@ class UpdateInstitutionProgramsIndex < ActiveRecord::Migration
   def change
     remove_index :institution_programs, name: "index_institution_programs_on_facility_code_and_description"
     add_index :institution_programs,
-              [:facility_code, :description,:version], 
+              [:facility_code, :description, :version], 
               unique: true, 
               algorithm: :concurrently, 
               name: 'index_institution_programs'
