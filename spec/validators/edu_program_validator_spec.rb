0da0a3454c432f5c6cc9b05275727f7f7a5929a7
Matt Roth <roth_matthew@bah.com>
2019-09-24 10:56:25 -0400

    add unit tests for validators

---
+++ spec/validators/edu_program_validator_spec.rb
@@ -0,0 +1,39 @@
+require 'rspec'
+
+describe EduProgramValidator do
+  describe 'when validating for load_csv context' do
+    it 'has no errors' do
+      edu_program = create :edu_program
+
+      expect(edu_program).to be_valid
+    end
+
+    it 'has invalid facility_code & vet_tec_program error message' do
+      edu_program = create :edu_program
+      edu_program_2 = create :edu_program, facility_code: edu_program.facility_code
+
+      expect(edu_program.valid?(:load_csv)).to eq(false)
+      expect(edu_program_2.valid?(:load_csv)).to eq(false)
+
+      error_messages = edu_program.errors.messages
+      expect(error_messages.any?).to eq(true)
+
+      error_message = "The Facility Code & VET TEC Program (Program Name) combination is not unique:
+#{edu_program.facility_code}, #{edu_program.vet_tec_program}"
+      expect(error_messages[:base]).to include(error_message)
+    end
+
+    it 'has invalid facility code error message' do
+      edu_program = create :edu_program, facility_code: 00
+
+      expect(edu_program.valid?(:load_csv)).to eq(false)
+
+      error_messages = edu_program.errors.messages
+      expect(error_messages.any?).to eq(true)
+
+      error_message = "The Facility Code #{edu_program.facility_code} is not contained within the most recently uploaded weams.csv"
+
+      expect(error_messages[:base]).to include(error_message)
+    end
+  end
+end
---
+++ spec/validators/program_validator_spec.rb
@@ -0,0 +1,39 @@
+require 'rspec'
+
+describe ProgramValidator do
+  describe 'when validating for load_csv context' do
+    it 'has no errors' do
+      program = create :program
+
+      expect(program).to be_valid
+    end
+
+    it 'has invalid facility code & description error message' do
+      program = create :program
+      program_2 = create :program, facility_code: program.facility_code
+
+      expect(program.valid?(:load_csv)).to eq(false)
+      expect(program_2.valid?(:load_csv)).to eq(false)
+
+      error_messages = program.errors.messages
+      expect(error_messages.any?).to eq(true)
+
+      error_message = "The Facility Code & Description (Program Name) combination is not unique:
+#{program.facility_code}, #{program.description}"
+      expect(error_messages[:base]).to include(error_message)
+    end
+
+    it 'has invalid facility code error message' do
+      program = create :program, facility_code: 00
+
+      expect(program.valid?(:load_csv)).to eq(false)
+
+      error_messages = program.errors.messages
+      expect(error_messages.any?).to eq(true)
+
+      error_message = "The Facility Code #{program.facility_code} is not contained within the most recently uploaded weams.csv"
+
+      expect(error_messages[:base]).to include(error_message)
+    end
+  end
+end
