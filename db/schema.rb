093561ac53f27e52dd540e72fc47c39f2fc3ea52
Devin McCurdy <mccurdy_devin@bah.com>
2019-10-08 13:33:42 -0400

    Merged master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190926141900) do
+ActiveRecord::Schema.define(version: 20190927174523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
