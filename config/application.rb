ebab2b47268b9cad67e2f439f030611e76fc6a74
Matt Roth <roth_matthew@bah.com>
2019-10-24 12:22:40 -0400

    add properties to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,19 @@ module GibctDataService
 
     # YAML Defaults for CSV
     config.csv_defaults = YAML.load_file(Rails.root.join('config', 'csv_file_defaults.yml'))
+
+    # React Settings
+    # Settings for the pool of renderers:
+    config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
+    config.react.server_renderer_timeout    ||= 20 # seconds
+    config.react.server_renderer = React::ServerRendering::BundleRenderer
+    config.react.server_renderer_options = {
+        files: ["server_rendering.js"],       # files to load for prerendering
+        replay_console: true,                 # if true, console.* will be replayed client-side
+    }
+    # Changing files matching these dirs/exts will cause the server renderer to reload:
+    config.react.server_renderer_extensions = ["jsx", "js"]
+    config.react.server_renderer_directories = ["/app/assets/javascripts", "/app/javascript/"]
+    config.react.camelize_props = true # default false
   end
 end
