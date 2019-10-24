5e9cab462bb25039a7e4abcd6b1dce01a7eda6d6
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-10-24 11:59:25 -0400

    Fixed Leaky Constant Declaration cop

--- spec/config/initializers/csv_types_spec.rb
+++ spec/config/initializers/csv_types_spec.rb
@@ -1,37 +1,36 @@
 # frozen_string_literal: true
 
+REQUIRED_TABLES = [
+  AccreditationAction.name,
+  AccreditationInstituteCampus.name,
+  AccreditationRecord.name,
+  ArfGiBill.name,
+  Complaint.name,
+  Crosswalk.name,
+  EightKey.name,
+  Hcm.name,
+  IpedsHd.name,
+  IpedsIcAy.name,
+  IpedsIcPy.name,
+  IpedsIc.name,
+  Mou.name,
+  Outcome.name,
+  P911Tf.name,
+  P911Yr.name,
+  Scorecard.name,
+  Sec702School.name,
+  Sec702.name,
+  Settlement.name,
+  Sva.name,
+  Vsoc.name,
+  Weam.name,
+  IpedsCipCode.name,
+  StemCipCode.name,
+  YellowRibbonProgramSource.name,
+  SchoolClosure.name,
+  Sec109ClosedSchool.name
+].freeze
 RSpec.describe 'CSV_TYPES' do
-  REQUIRED_TABLES = [
-    AccreditationAction.name,
-    AccreditationInstituteCampus.name,
-    AccreditationRecord.name,
-    ArfGiBill.name,
-    Complaint.name,
-    Crosswalk.name,
-    EightKey.name,
-    Hcm.name,
-    IpedsHd.name,
-    IpedsIcAy.name,
-    IpedsIcPy.name,
-    IpedsIc.name,
-    Mou.name,
-    Outcome.name,
-    P911Tf.name,
-    P911Yr.name,
-    Scorecard.name,
-    Sec702School.name,
-    Sec702.name,
-    Settlement.name,
-    Sva.name,
-    Vsoc.name,
-    Weam.name,
-    IpedsCipCode.name,
-    StemCipCode.name,
-    YellowRibbonProgramSource.name,
-    SchoolClosure.name,
-    Sec109ClosedSchool.name
-  ].freeze
-
   describe 'all_tables' do
     it 'lengths should be equal' do
       expect(CSV_TYPES_ALL_TABLES.length).to eq(CSV_TYPES_TABLES.length)
