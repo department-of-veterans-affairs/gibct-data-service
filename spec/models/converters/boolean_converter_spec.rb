62acc3a85ae60801301febd40f9881e7bf4e7b09
Joe <elder_joseph@bah.com>
2019-10-28 12:31:49 -0400

    19832: CT: Address RSpec/NamedSubject rubocop errors in GIDS: Correct boolean converter

--- spec/models/converters/boolean_converter_spec.rb
+++ spec/models/converters/boolean_converter_spec.rb
@@ -3,25 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe BooleanConverter do
-  subject(:deez) { described_class }
+  subject { described_class }
 
   it 'converts strings to booleans' do
     %w[TruE T yes ye y 1 on].each do |value|
-      expect(deez.convert(value)).to be_truthy
+      expect(described_class.convert(value)).to be_truthy
     end
   end
 
   it 'converts numbers to booleans' do
-    expect(deez.convert(1)).to be_truthy
-    expect(deez.convert(2)).to be_falsey
+    expect(described_class.convert(1)).to be_truthy
+    expect(described_class.convert(2)).to be_falsey
   end
 
   it 'converts non-truthy strings to falsey' do
-    expect(deez.convert('some random string')).to be_falsey
+    expect(described_class.convert('some random string')).to be_falsey
   end
 
   it 'converts blanks to nil' do
-    expect(deez.convert(nil)).to be_nil
-    expect(deez.convert('      ')).to be_nil
+    expect(described_class.convert(nil)).to be_nil
+    expect(described_class.convert('      ')).to be_nil
   end
 end
