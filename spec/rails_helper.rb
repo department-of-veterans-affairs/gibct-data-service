39e65cf2df97ac4b28fd1834860b87a95cc75d48
Matt Roth <roth_matthew@bah.com>
2019-10-28 11:45:29 -0400

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ require 'support/site_mapper_helper'
 ActiveRecord::Migration.maintain_test_schema!
 
 require 'capybara/rspec'
-Capybara.default_driver = :sniffybara
+# Capybara.default_driver = :sniffybara
 Capybara.javascript_driver = :webkit_debug
 
 RSpec.configure do |config|
