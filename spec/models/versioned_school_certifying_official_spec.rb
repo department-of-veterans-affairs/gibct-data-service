f889d04c49fc7c563159bd48137fa4ed0052c9e3
Joe <elder_joseph@bah.com>
2019-10-28 12:48:27 -0400

    19832: CT: Address RSpec/NamedSubject rubocop errors in GIDS: Correct versioned sco spec

--- spec/models/versioned_school_certifying_official_spec.rb
+++ spec/models/versioned_school_certifying_official_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe VersionedSchoolCertifyingOfficial, type: :model do
   describe 'when validating' do
-    subject(versioned_school_certifying_official) { build :versioned_school_certifying_official }
+    subject(:versioned_school_certifying_official) { build :versioned_school_certifying_official }
 
     it 'has a valid factory' do
       expect(versioned_school_certifying_official).to be_valid
