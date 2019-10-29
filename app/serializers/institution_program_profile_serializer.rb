e97a5ea83f61c2891c80be629514d949a5f6fc8c
Devin McCurdy <mccurdy_devin@bah.com>
2019-10-29 15:01:55 -0400

    Add new InstitutionProgram serializer for institution profile

---
+++ app/serializers/institution_program_profile_serializer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class InstitutionProgramProfileSerializer < ActiveModel::Serializer
+  attributes :program_type,
+             :description,
+             :length_in_hours,
+             :length_in_weeks,
+             :provider_website,
+             :phone_area_code,
+             :phone_number,
+             :school_locale,
+             :tuition_amount,
+             :va_bah,
+             :dod_bah
+end
