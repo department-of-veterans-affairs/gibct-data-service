43c7ef390a3ed29e55f9ea3718fc7cf6f946598f
hughes-dustin <hughes_dustin@bah.com>
2019-10-29 11:23:08 -0400

    update rubocop todo

--- spec/controllers/v0/institutions_controller_spec.rb
+++ spec/controllers/v0/institutions_controller_spec.rb
@@ -97,6 +97,7 @@ RSpec.describe V0::InstitutionsController, type: :controller do
       expect(facets['principles_of_excellence'].keys).to include('true', 'false')
       expect(facets['eight_keys_to_veteran_success'].keys).to include('true', 'false')
     end
+
     def check_boolean_facets_2(facets)
       expect(facets['stem_offered'].keys).to include('true', 'false')
       expect(facets['independent_study'].keys).to include('true', 'false')
