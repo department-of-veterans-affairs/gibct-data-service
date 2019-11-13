dd71609a20d46b7bdc1fa24c2fdaaba47b7cdcee
Matt Roth <roth_matthew@bah.com>
2019-11-13 11:08:10 -0500

    AC2: If any of the following fields are null or empty on upload a warning message is displayed stating that the row will not be able to be matched with a corresponding row: Facility Code VET TEC Program

--- app/models/edu_program.rb
+++ app/models/edu_program.rb
@@ -20,6 +20,5 @@ class EduProgram < ApplicationRecord
     'program length' => { column: :length_in_weeks, converter: NumberConverter }
   }.freeze
 
-  validates :facility_code, presence: true
   validates_with EduProgramValidator, on: :after_import
 end
