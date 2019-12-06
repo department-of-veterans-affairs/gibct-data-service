9763463520ace9643fd0ff30d4fb9d5f30ff928a
Matt Roth <roth_matthew@bah.com>
2019-12-06 15:49:28 -0500

    cleanup ProgramValidator

--- app/validators/program_validator.rb
+++ app/validators/program_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ProgramValidator < BaseValidator
+class ProgramValidator
   def self.after_import_batch_validations(failed_instances)
     duplicate_facility_description_results.each do |record|
       record.errors[:base] << non_unique_error_msg(record)
@@ -8,7 +8,7 @@ class ProgramValidator < BaseValidator
     end
 
     missing_facility_in_weam.each do |record|
-      record.errors[:base] << missing_facility_error_msg(record)
+      record.errors[:base] << SharedMessages.missing_facility_error_msg(record)
       add_record_to_failed_instances(record, failed_instances)
     end
   end
