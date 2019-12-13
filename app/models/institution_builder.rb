2ca156e976b5062c9b7d16b4756d17b0bb795ba9
Joe <elder_joseph@bah.com>
2019-12-13 12:51:47 -0500

    4063: Setup parent/child version relationships for institutions and zip code rates

--- app/models/institution_builder.rb
+++ app/models/institution_builder.rb
@@ -50,6 +50,7 @@ module InstitutionBuilder
     build_zip_code_rates_from_weams(version_number)
     build_institution_programs(version_number)
     build_versioned_school_certifying_official(version_number)
+    add_version_id(version_number)
   end
 
   def self.run(user)
@@ -473,7 +474,8 @@ module InstitutionBuilder
       mha_name,
       version,
       created_at,
-      updated_at
+      updated_at,
+      version_id
     )
     SELECT
       zip,
@@ -482,16 +484,17 @@ module InstitutionBuilder
       concat_ws(', ', physical_city, physical_state) as physical_location,
       ?,
       #{conn.quote(timestamp)},
-      #{conn.quote(timestamp)}
-    FROM weams
+      #{conn.quote(timestamp)},
+      v.id
+      FROM weams JOIN versions v ON v.number = ?
     WHERE country = 'USA'
       AND bah IS NOT null
       AND dod_bah IS NOT null
-    GROUP BY zip, bah, dod_bah, physical_location
+    GROUP BY zip, bah, dod_bah, physical_location, v.id
     ORDER BY zip
     SQL
 
-    sql = ZipcodeRate.send(:sanitize_sql, [str, version_number])
+    sql = ZipcodeRate.send(:sanitize_sql, [str, version_number, version_number])
     ZipcodeRate.connection.execute(sql)
   end
 
@@ -607,5 +610,16 @@ module InstitutionBuilder
 
     sql = SchoolCertifyingOfficial.send(:sanitize_sql, [str, version_number])
     SchoolCertifyingOfficial.connection.execute(sql)
+  end
+
+  def self.add_version_id(version_number)
+    str = <<-SQL
+      UPDATE institutions SET
+        version_id = versions.id
+      FROM versions
+      WHERE versions.number = #{version_number};
+    SQL
+
+    Institution.connection.update(str)
   end
 end
