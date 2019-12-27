dda2012bc87cc043b30c6c075b938cecac37196c
Joe <elder_joseph@bah.com>
2019-12-27 14:54:36 -0500

    4063: Add indexing for version_id, update AR migration version

--- db/migrate/20191211111000_add_version_id_column_to_zipcode_rates.rb
+++ db/migrate/20191211111000_add_version_id_column_to_zipcode_rates.rb
@@ -1,5 +1,8 @@
-class AddVersionIdColumnToZipcodeRates < ActiveRecord::Migration[5.1]
+class AddVersionIdColumnToZipcodeRates < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_column :zipcode_rates, :version_id, :integer
+    add_reference :zipcode_rates, :version, foreign_key: true, index: false
+    add_index :zipcode_rates, :version_id, algorithm: :concurrently
   end
 end
--- db/migrate/20191211111001_add_version_id_column_to_zipcode_rates_archives.rb
+++ db/migrate/20191211111001_add_version_id_column_to_zipcode_rates_archives.rb
@@ -1,5 +1,8 @@
-class AddVersionIdColumnToZipcodeRatesArchives < ActiveRecord::Migration[5.1]
+class AddVersionIdColumnToZipcodeRatesArchives < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_column :zipcode_rates_archives, :version_id, :integer
+    add_reference :zipcode_rates_archives, :version, foreign_key: true, index: false
+    add_index :zipcode_rates_archives, :version_id, algorithm: :concurrently
   end
 end
