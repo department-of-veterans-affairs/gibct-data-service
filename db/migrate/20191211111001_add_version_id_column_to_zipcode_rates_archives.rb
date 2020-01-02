b8e7c02818b6bd64fd3256a70e625b16c5be7f66
Joe <elder_joseph@bah.com>
2020-01-02 16:18:50 -0500

    Remove indexing for zipcode_rates_archives and institutions_archives

--- db/migrate/20191211111001_add_version_id_column_to_zipcode_rates_archives.rb
+++ db/migrate/20191211111001_add_version_id_column_to_zipcode_rates_archives.rb
@@ -1,8 +1,5 @@
 class AddVersionIdColumnToZipcodeRatesArchives < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
   def change
-    add_reference :zipcode_rates_archives, :version, foreign_key: true, index: false
-    add_index :zipcode_rates_archives, :version_id, algorithm: :concurrently
+    add_column :zipcode_rates_archives, :version_id, :integer
   end
 end
--- db/migrate/20191211111003_add_version_id_column_to_institutions_archives.rb
+++ db/migrate/20191211111003_add_version_id_column_to_institutions_archives.rb
@@ -1,8 +1,5 @@
 class AddVersionIdColumnToInstitutionsArchives < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
   def change
-    add_reference :institutions_archives, :version, foreign_key: true, index: false
-    add_index :institutions_archives, :version_id, algorithm: :concurrently
+    add_column :institutions_archives, :version_id, :integer
   end
 end
