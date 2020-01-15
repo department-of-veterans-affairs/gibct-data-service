7e9b5246c00c21d9455c1cd2ace5e90a3c373907
Matt Roth <roth_matthew@bah.com>
2020-01-15 11:20:54 -0500

    fix a test

--- spec/support/configuration_helper.rb
+++ spec/support/configuration_helper.rb
@@ -39,6 +39,13 @@ module Specs
       end
 
       class ServiceException < StandardError
+        attr_accessor :key, :response_values, :original_status, :original_body
+        def initialize(key, response_values, original_status, original_body)
+          self.key = key
+          self.response_values = response_values
+          self.original_status = original_status
+          self.original_body = original_body
+        end
       end
 
       module Configuration
