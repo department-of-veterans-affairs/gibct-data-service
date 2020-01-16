8dc4298760b5fbc02c7ae52d7805f3b64fb55c1a
Joe <elder_joseph@bah.com>
2020-01-15 19:17:54 -0500

    4038: Correct Zip Code Rate Queries and Unit tests

--- app/controllers/v0/zipcode_rates_controller.rb
+++ app/controllers/v0/zipcode_rates_controller.rb
@@ -4,8 +4,9 @@ module V0
   class ZipcodeRatesController < ApiController
     # GET /v0/zipcode_rates/20001
     def show
-      resource = ZipcodeRate.where(version: @version[:number], zip_code: params[:id]).order(:mha_rate).first
-
+      resource = ZipcodeRate.joins("INNER JOIN versions v ON v.number = #{@version[:number]}")
+                            .where('zipcode_rates.version_id = v.id')
+                            .where(zip_code: params[:id]).order(:mha_rate).first
       raise Common::Exceptions::RecordNotFound, params[:id] unless resource
 
       render json: resource, serializer: ZipcodeRateSerializer
