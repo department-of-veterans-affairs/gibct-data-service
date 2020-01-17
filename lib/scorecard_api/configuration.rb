7f043dddf3e33e70dda5a5b6b442425ec0cd4367
Matt Roth <roth_matthew@bah.com>
2020-01-17 15:42:04 -0500

    add autoloading for lib directory

--- lib/scorecard_api/configuration.rb
+++ lib/scorecard_api/configuration.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'common/client/configuration/rest'
 require 'common/client/middleware/response/scorecard_api_errors'
 require 'common/client/middleware/response/json_parser'
 require 'common/client/middleware/response/raise_error'
