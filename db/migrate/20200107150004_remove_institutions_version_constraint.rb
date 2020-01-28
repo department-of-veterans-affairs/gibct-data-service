668c9af1a7f2deb2c6780fdb0c452a05d63e3199
Joe <elder_joseph@bah.com>
2020-01-28 14:38:39 -0500

    4038: Make changes based off of review

--- db/migrate/20200107150004_remove_institutions_version_constraint.rb
+++ db/migrate/20200107150004_remove_institutions_version_constraint.rb
@@ -1,8 +1,7 @@
 
 class RemoveInstitutionsVersionConstraint < ActiveRecord::Migration[5.2]
-    disable_ddl_transaction!
-
-    def change
-        change_column_null :institutions, :version, true
-    end
+  disable_ddl_transaction!
+  def change
+    change_column_null :institutions, :version, true
   end
+end
