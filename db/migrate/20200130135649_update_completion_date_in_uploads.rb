department-of-veterans-affairs/gibct-data-service

commit 70e2bfe1b3379d3c4a1c04e9eeddedc0e7d06b37

GIDS: Update dashboard after completed Scorecard API request: DB (#543)
DB changes for https://github.com/department-of-veterans-affairs/gibct-data-service/pull/547

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_210003) do
+ActiveRecord::Schema.define(version: 2020_01_24_135649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1425,6 +1425,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_210003) do
     t.boolean "ok", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["csv_type"], name: "index_uploads_on_csv_type"
     t.index ["updated_at"], name: "index_uploads_on_updated_at"
     t.index ["user_id"], name: "index_uploads_on_user_id"

db/migrate/20200124135649_add_completion_date_to_uploads.rb
@@ -1,0 +1,8 @@
+class AddCompletionDateToUploads < ActiveRecord::Migration[5.2]
+  def change
+    add_column :uploads, :completed_at, :datetime
+    reversible do |dir|
+      dir.up { Upload.where(ok: true).update_all('completed_at = updated_at') }
+    end
+  end
+end

commit 64beff3399c34f857a58ba3f67836b0bd7881de9

User receives "Scorecard is already being fetched by another user" error when no other user is fetching #5312 (#554)
* fix issue caused in https://github.com/department-of-veterans-affairs/gibct-data-service/pull/543

* account for only columns without completed_at date

db/migrate/20200130135649_update_completion_date_in_uploads.rb
@@ -1,0 +1,7 @@
+class UpdateCompletionDateInUploads < ActiveRecord::Migration[5.2]
+  def change
+    reversible do |dir|
+      dir.up { Upload.where(completed_at: nil).update_all('completed_at = updated_at') }
+    end
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_135649) do
+ActiveRecord::Schema.define(version: 2020_01_30_135649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"