21c4c08b557470450e6f5e0f6ae0e0cedbf197a1
Devin McCurdy <mccurdy_devin@bah.com>
2020-02-03 14:57:02 -0500

    Add institution address indexes

--- db/migrate/20200203122200_add_trigram_indexes_to_institutions.rb
+++ db/migrate/20200203122200_add_trigram_indexes_to_institutions.rb
@@ -5,6 +5,9 @@ class AddTrigramIndexesToInstitutions < ActiveRecord::Migration[5.2]
     safety_assured do
       execute "CREATE INDEX CONCURRENTLY index_institutions_on_institution_trgm ON institutions USING gin(institution gin_trgm_ops);"
       execute "CREATE INDEX CONCURRENTLY index_institutions_on_city_trgm ON institutions USING gin(city gin_trgm_ops);"
+      execute "CREATE INDEX CONCURRENTLY index_institutions_on_address_1_trgm ON institutions USING gin(LOWER(address_1) gin_trgm_ops);"
+      execute "CREATE INDEX CONCURRENTLY index_institutions_on_address_2_trgm ON institutions USING gin(LOWER(address_2) gin_trgm_ops);"
+      execute "CREATE INDEX CONCURRENTLY index_institutions_on_address_3_trgm ON institutions USING gin(LOWER(address_3) gin_trgm_ops);"
     end
   end
 
@@ -12,6 +15,9 @@ class AddTrigramIndexesToInstitutions < ActiveRecord::Migration[5.2]
     safety_assured do
       execute "DROP INDEX index_institutions_on_institution_trgm;"
       execute "DROP INDEX index_institutions_on_city_trgm;"
+      execute "DROP INDEX index_institutions_on_address_1_trgm;"
+      execute "DROP INDEX index_institutions_on_address_2_trgm;"
+      execute "DROP INDEX index_institutions_on_address_3_trgm;"
     end
   end
 end
