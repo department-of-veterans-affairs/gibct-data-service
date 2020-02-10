c80710b1df4c48ff64bd62ee19d4044c6fea46b7
Matt Roth <roth_matthew@bah.com>
2020-02-10 10:51:03 -0500

    GIDS: Order crosswalk issues by number of GI bill students. #5342 (#560)
    As an Education Services Team Member, I need the Crosswalk data discrepancies to sort the facilities descending by the number of GI Bill students at the facility so that I can prioritize fixing the facilities with the most students.
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/5342

--- app/controllers/crosswalk_issues_controller.rb
+++ app/controllers/crosswalk_issues_controller.rb
@@ -2,9 +2,9 @@
 
 class CrosswalkIssuesController < ApplicationController
   def partials
-    @issues = CrosswalkIssue.includes(%i[weam crosswalk ipeds_hd])
+    @issues = CrosswalkIssue.includes(:crosswalk, :ipeds_hd, weam: :arf_gi_bill)
                             .by_issue_type(CrosswalkIssue::PARTIAL_MATCH_TYPE)
-                            .order('weams.institution, weams.facility_code')
+                            .order('arf_gi_bills.gibill desc nulls last, weams.institution, weams.facility_code')
   end
 
   def show_partial
