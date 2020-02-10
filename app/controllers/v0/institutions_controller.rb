521d5726e1e1a8fc77d80e4f787cd2d1ae1db867
Joe <elder_joseph@bah.com>
2020-02-10 16:39:38 -0500

    4038: Add search by version

--- app/controllers/v0/institutions_controller.rb
+++ app/controllers/v0/institutions_controller.rb
@@ -45,6 +45,7 @@ module V0
     # GET /v0/institutions/20005123/children
     def children
       children = Institution.joins(:version)
+                            .where(version: @version)
                             .where(parent_facility_code_id: params[:id])
                             .order(:institution)
                             .page(params[:page])
@@ -140,7 +141,7 @@ module V0
     end
 
     def approved_institutions
-      Institution.joins(:version).no_extentions.where(approved: true)
+      Institution.joins(:version).no_extentions.where(approved: true, version: @version)
     end
   end
   # rubocop:enable Metrics/ClassLength
--- app/controllers/v0/zipcode_rates_controller.rb
+++ app/controllers/v0/zipcode_rates_controller.rb
@@ -5,7 +5,7 @@ module V0
     # GET /v0/zipcode_rates/20001
     def show
       resource = ZipcodeRate.joins(:version)
-                            .where(zip_code: params[:id]).order(:mha_rate).first
+                            .where(zip_code: params[:id], version: @version).order(:mha_rate).first
       raise Common::Exceptions::RecordNotFound, params[:id] unless resource
 
       render json: resource, serializer: ZipcodeRateSerializer
