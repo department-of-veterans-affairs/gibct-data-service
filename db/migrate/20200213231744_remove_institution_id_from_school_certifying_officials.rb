7e4cf21a0a3f1bb88e6c2e8a96da5845da84f197
Joe <elder_joseph@bah.com>
2020-02-18 19:23:50 -0500

    4038: Temporarily remove migration to remove institution_id from school_certifying_officials

--- db/migrate/20200213231744_remove_institution_id_from_school_certifying_officials.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInstitutionIdFromSchoolCertifyingOfficials < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured { remove_column :school_certifying_officials, :institution_id, :bigint }
-  end
-end
