2559bfdc1a5e192152770c164be4eb854bb58906
Kelson Adams <kadams@governmentcio.com>
2020-02-25 08:32:28 -0700

    Omit unused lambdas and validations from YRPS

--- app/models/yellow_ribbon_program_source.rb
+++ app/models/yellow_ribbon_program_source.rb
@@ -43,50 +43,11 @@ class YellowRibbonProgramSource < ApplicationRecord
     'year of yr participation' => { column: :year_of_yr_participation }
   }.freeze
 
-  # validates :city, presence: true
   validates :contribution_amount, numericality: true
   validates :degree_level, presence: true
   validates :division_professional_school, presence: true
   validates :facility_code, presence: true
   validates :number_of_students, numericality: true
-  # validates :school_name_in_yr_database, presence: true
-  # validates :state, presence: true
-  # validates :street_address, presence: true
-  # validates :zip, presence: true
-
-  # Finds exact-matching facility_code or partial-matching school and city names
-  scope :search, lambda { |search_term|
-    return if search_term.blank?
-
-    clause = [
-      'lower(school_name_in_yr_database) LIKE (:search_term)'
-    ]
-
-    where(
-      sanitize_sql_for_conditions(
-        [clause.join(' OR '),
-         search_term: "%#{search_term}%"]
-      )
-    )
-  }
-
-  scope :filter, lambda { |field, value|
-    return if value.blank?
-    raise ArgumentError, 'Field name is required' if field.blank?
-
-    case value
-    when 'true', 'yes'
-      where(field => true)
-    when 'false', 'no'
-      where.not(field => true)
-    else
-      where(field => value)
-    end
-  }
 
   scope :version, ->(n) { where(version: n) }
-
-  scope :filter_count, lambda { |field|
-    group(field).where.not(field => nil).order(field).count
-  }
 end
