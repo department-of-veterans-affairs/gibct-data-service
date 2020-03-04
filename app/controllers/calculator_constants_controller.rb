1575dd0299704805587b4ce22bd95db970f44167
hughes-dustin <hughes_dustin@bah.com>
2020-03-04 11:16:16 -0500

    update upload date when saving calculator constants

--- app/controllers/calculator_constants_controller.rb
+++ app/controllers/calculator_constants_controller.rb
@@ -6,8 +6,16 @@ class CalculatorConstantsController < ApplicationController
   end
 
   def update
+    calculator_constant_upload = Upload.where(csv_type: 'CalculatorConstant').first
     params.each do |key, value|
-      CalculatorConstant.where(name: key).first.update(float_value: value) if CalculatorConstant.exists?(name: key)
+      next unless CalculatorConstant.exists?(name: key)
+
+      constant_field = CalculatorConstant.where(name: key).first
+      constant_field.update(float_value: value)
+      updated_timestamp = constant_field.updated_at
+      if updated_timestamp > calculator_constant_upload.updated_at
+        calculator_constant_upload.update(updated_at: updated_timestamp, created_at: updated_timestamp)
+      end
     end
     flash.notice = 'Calculator Constants have been updated'
     redirect_to action: :index
