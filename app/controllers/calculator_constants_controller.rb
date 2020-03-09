ca2641ba4186fcfa9eeed6828c8c2ab5fd4d053a
hughes-dustin <hughes_dustin@bah.com>
2020-03-09 12:05:43 -0400

    update upload to include user data

--- app/controllers/calculator_constants_controller.rb
+++ app/controllers/calculator_constants_controller.rb
@@ -6,13 +6,13 @@ class CalculatorConstantsController < ApplicationController
   end
 
   def update
-    upload = Upload.where(csv_type: 'CalculatorConstant').first
+    upload = Upload.where({ csv_type: 'CalculatorConstant', ok: true }).order(updated_at: :desc).first
     params.each do |key, value|
       next unless CalculatorConstant.exists?(name: key)
 
       constant_field = CalculatorConstant.where(name: key).first
       constant_field.update(float_value: value)
-      update_upload_timestamp(upload, constant_field.updated_at)
+      update_calculator_constant_upload(upload, constant_field.updated_at)
     end
     flash.notice = 'Calculator Constants have been updated.'
     redirect_to action: :index
@@ -21,6 +21,9 @@ end
 
 private
 
-def update_upload_timestamp(upload, updated_timestamp)
-  upload.update(updated_at: updated_timestamp, created_at: updated_timestamp) if updated_timestamp > upload.updated_at
+def update_calculator_constant_upload(upload, updated_timestamp)
+  if updated_timestamp > upload.updated_at
+    upload.update(updated_at: updated_timestamp, created_at: updated_timestamp,
+                  completed_at: updated_timestamp, user: current_user)
+  end
 end
