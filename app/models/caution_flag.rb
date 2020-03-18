2d108d9b940273d45e6b5a9947fa215487c073ae
Matt Roth <roth_matthew@bah.com>
2020-03-18 13:16:49 -0400

    fix issue with archive export

--- app/models/caution_flag.rb
+++ app/models/caution_flag.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CautionFlag < ApplicationRecord
+  belongs_to :institution
   SOURCES = {
     accreditation_action: 'accreditation_action',
     mou: 'mou',
--- app/models/institutions_archive.rb
+++ app/models/institutions_archive.rb
@@ -2,4 +2,14 @@
 
 class InstitutionsArchive < Institution
   self.table_name = 'institutions_archives'
+
+  # method is to support frontend until has been switched over to using caution_flags attribute
+  def caution_flag
+    false
+  end
+
+  # method is to support frontend until has been switched over to using caution_flags attribute
+  def caution_flag_reason
+    ''
+  end
 end
