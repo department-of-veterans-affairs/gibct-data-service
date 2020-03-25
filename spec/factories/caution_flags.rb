b20eeb08569997ddd770dd113f3aa87b39ed8129
Devin McCurdy <mccurdy_devin@bah.com>
2020-03-25 14:36:56 -0400

    Fix linter error

--- spec/factories/caution_flags.rb
+++ spec/factories/caution_flags.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     trait :accreditation_issue do
       source { 'accreditation' }
       title { 'School has an accreditation issue' }
-      description { "This school's accreditation has been revoked and is under appeal, or the school has been placed on probation as it didn't meet acceptable levels of quality." }
+      description { "This school's accreditation has been revoked and is under appeal." }
       link_text { "Learn more about this school's accreditation" }
       link_url { 'http://ope.ed.gov/accreditation' }
     end
