2b80ab2b3ea43e57393b904fb8244abc59e32d31
Matt Roth <roth_matthew@bah.com>
2020-03-31 12:04:36 -0400

    get distinct predicates only for rule_name CautionFlag, make sure that source rules are applied before reason rules as generic to specific, add comments

--- app/models/caution_flag.rb
+++ app/models/caution_flag.rb
@@ -3,32 +3,42 @@
 class CautionFlag < ApplicationRecord
   belongs_to :institution
 
-  COLS_MAP_UPDATE = %i[
-    title description link_text link_url
-  ].freeze
-
   def self.map(version_id)
     puts "START #{Time.now}"
     engine = Rule.create_engine
 
-    predicates = Rule.distinct.pluck(:predicate)
-    caution_flags = where(version_id: version_id)
+    # Get distinct predicates
+    predicates = Rule.select(:predicate).where(rule_name: CautionFlag.name).distinct.pluck(:predicate)
 
-    CautionFlagRule.all.find_each do |cf_rule|
-      caution_flags.each do |cf|
-        predicates.each do |predicate|
-          engine << [cf.id, predicate.to_sym, cf[predicate.to_sym]]
-        end
+    # Load all caution flag rows into engine for each predicate
+    where(version_id: version_id).each do |cf|
+      predicates.map(&:to_sym).each do |predicate|
+        engine << [cf.id, predicate, cf[predicate]]
       end
+    end
 
+    # Apply rules for rules that use source as a predicate as these are more general
+    CautionFlagRule.includes(:rule)
+        .where('rules.predicate = ?', 'source').references(:rule).each do |cf_rule|
       subjects = Rule.apply_rule(engine, cf_rule.rule)
       apply_update(cf_rule, subjects) unless subjects.empty?
     end
+
+    # Apply rules for rules that use reason as a predicate as these are more specific
+    CautionFlagRule.includes(:rule)
+        .where('rules.predicate = ?', 'reason').references(:rule).each do |cf_rule|
+      subjects = Rule.apply_rule(engine, cf_rule.rule)
+      apply_update(cf_rule, subjects) unless subjects.empty?
+    end
+
     puts "END #{Time.now}"
   end
 
   def self.apply_update(rule, ids)
-    cols_to_update = COLS_MAP_UPDATE.map(&:to_s)
+    cols_map_update = %i[
+      title description link_text link_url
+    ]
+    cols_to_update = cols_map_update.map(&:to_s)
                                     .map { |col| %(#{col} = #{CautionFlagRule.table_name}.#{col}) }.join(', ')
 
     str = <<-SQL
