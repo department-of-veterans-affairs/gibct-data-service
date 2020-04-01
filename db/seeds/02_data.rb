58ecc0c0f55ecaa849a5293c4da10ba2a526bd28
Matt Roth <roth_matthew@bah.com>
2020-04-01 14:23:34 -0400

    ensure preview generation and setting to production always occurs last

--- db/seeds/02_data.rb
+++ db/seeds/02_data.rb
@@ -65,22 +65,6 @@ if ENV['CI'].blank?
   SeedUtils.seed_table_with_upload(SchoolCertifyingOfficial, user)
   SeedUtils.seed_table_with_upload(EduProgram, user)
   SeedUtils.seed_table_with_upload(Sec103, user)
-
-  puts 'Building Institutions'
-  result = InstitutionBuilder.run(user)
-
-  if result[:success]
-    puts "Setting version: #{result[:version].number} as production"
-    version = Version.current_preview
-    version.update(production: true)
-  else
-    puts "Error occurred: #{result[:notice]}: #{result[:error_msg]}"
-  end
-
-  puts 'Building CrosswalkIssues'
-  CrosswalkIssue.rebuild
-
-  puts "Done ... Woo Hoo!"
 end
 
 
