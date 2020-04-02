f3367c51598d4115037889e7d53f09d55e670136
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-04-02 14:38:43 -0400

    caution flag filter migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_092001) do
+ActiveRecord::Schema.define(version: 2020_04_02_101010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -408,6 +408,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_092001) do
     t.boolean "complies_with_sec_103"
     t.boolean "solely_requires_coe"
     t.boolean "requires_coe_and_criteria"
+    t.integer "count_of_caution_flags", default: 0
     t.index "lower((address_1)::text) gin_trgm_ops", name: "index_institutions_on_address_1", using: :gin
     t.index "lower((address_2)::text) gin_trgm_ops", name: "index_institutions_on_address_2", using: :gin
     t.index "lower((address_3)::text) gin_trgm_ops", name: "index_institutions_on_address_3", using: :gin
@@ -554,6 +555,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_092001) do
     t.boolean "complies_with_sec_103"
     t.boolean "solely_requires_coe"
     t.boolean "requires_coe_and_criteria"
+    t.integer "count_of_caution_flags", default: 0
   end
 
   create_table "ipeds_cip_codes", id: :serial, force: :cascade do |t|
