ef610feb2455ec7db3cd3d3e27fa6f8c81609036
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-04-02 13:36:17 -0400

    removed migration

--- db/migrate/20200402101010_add_count_of_caution_flags_column_to_institutions_archives.rb
+++
@@ -1,5 +0,0 @@
-class AddCountOfCautionFlagsColumnToInstitutionsArchives < ActiveRecord::Migration[5.2]
-    def change
-        add_column :institutions_archives, :count_of_caution_flags, :integer, :default => 0
-      end
-  end
