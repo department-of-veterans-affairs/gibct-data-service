ccf6233f429c6ba1448e082bea0564891bd3d8ed
Matt Roth <roth_matthew@bah.com>
2020-04-21 17:52:35 -0400

    use change_column_default

--- db/migrate/20200420194305_set_defaults_caution_flags.rb
+++ db/migrate/20200420194305_set_defaults_caution_flags.rb
@@ -1,19 +1,11 @@
 class SetDefaultsCautionFlags < ActiveRecord::Migration[5.2]
   def up
-    safety_assured do
-      change_table :caution_flags do |t|
-        t.change :title, :string, default: 'School engaged in misleading, deceptive, or erroneous practices'
-        t.change :description, :string, default: 'VA has found that this school engaged in misleading, deceptive, or erroneous advertising, sales, or enrollment practices, and has taken action against it.'
-      end
-    end
+    change_column_default :caution_flags,:title,'School engaged in misleading, deceptive, or erroneous practices'
+    change_column_default :caution_flags, :description,'VA has found that this school engaged in misleading, deceptive, or erroneous advertising, sales, or enrollment practices, and has taken action against it.'
   end
 
   def down
-    safety_assured do
-      change_table :caution_flags do |t|
-        t.change :title, :string, default: nil
-        t.change :description, :string, default: nil
-      end
-    end
+    change_column_default :caution_flags,:title,nil
+    change_column_default :caution_flags, :description,nil
   end
 end
