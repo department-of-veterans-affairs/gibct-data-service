dcf21017915b37c6ef62e346703e7a0d92fee7e9
hughes-dustin <48804654+hughes-dustin@users.noreply.github.com>
2020-04-29 09:59:49 -0400

    Add closed schools to search results caution flag filter (#648)
    * include school closure flag in caution flags count
    
    * school closing filter
    
    * remove formatted changes from linter
    
    * rename filter field
    
    * add in prod flag for changes
    
    * fix linter errors
    
    * fix institution programs filter
    
    * add back in filter so not to break vets-website prod build
    
    * fix error

--- app/controllers/v0/institution_programs_controller.rb
+++ app/controllers/v0/institution_programs_controller.rb
@@ -81,8 +81,10 @@ module V0
         relation = relation.filter(filter_args[0], @query[filter_args[1]])
       end
 
+      if @query[:exclude_warnings]
+        relation = relation.where('institutions.count_of_caution_flags = 0  AND institutions.school_closing IS FALSE')
+      end
       relation = relation.where(institutions: { count_of_caution_flags: 0 }) if @query[:exclude_caution_flags]
-
       relation
     end
 
