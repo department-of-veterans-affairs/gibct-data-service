977a38fa92107c7cef07d1732893051fad579ff0
Devin McCurdy <mccurdy_devin@bah.com>
2020-05-13 15:44:13 -0400

    Only generate Versioned SCOs for allowed priority values (#655)

--- app/models/versioned_school_certifying_official.rb
+++ app/models/versioned_school_certifying_official.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class VersionedSchoolCertifyingOfficial < ApplicationRecord
+  VALID_PRIORITY_VALUES = %w[
+    PRIMARY
+    SECONDARY
+  ].freeze
+
   belongs_to :institution
 end
