ae664ba93c72d1f68276262183f990b671774e32
Matt Roth <roth_matthew@bah.com>
2020-07-13 10:14:55 -0400

    linting

--- app/utilities/scorecard_api/service.rb
+++ app/utilities/scorecard_api/service.rb
@@ -23,12 +23,12 @@ module ScorecardApi
       'latest.aid.federal_loan_rate': :pctfloan,
       'latest.aid.median_debt_suppressed.completers.overall': :avg_stu_loan_debt,
       'latest.completion.rate_suppressed.four_year': :c150_4_pooled_supp,
-      'latest.completion.rate_suppressed.lt_four_year_150percent': :c150_l4_pooled_supp
+      'latest.completion.rate_suppressed.lt_four_year_150percent': :c150_l4_pooled_supp,
       'latest.earnings.10_yrs_after_entry.median': :salary_all_students,
       'latest.repayment.3_yr_repayment_suppressed.overall': :repayment_rate_all_students,
       'latest.student.retention_rate.four_year.full_time': :retention_all_students_ba,
       'latest.student.retention_rate.lt_four_year.full_time': :retention_all_students_otb,
-      'latest.student.size': :undergrad_enrollment,
+      'latest.student.size': :undergrad_enrollment
     }.freeze
 
     def self.populate
