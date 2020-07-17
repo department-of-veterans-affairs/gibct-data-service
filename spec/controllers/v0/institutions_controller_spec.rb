fbda87665287e798570d3b63eca12e8b76240342
Devin McCurdy <mccurdy_devin@bah.com>
2020-07-17 10:50:01 -0400

    Update fuzzy-search order by (#680)

--- spec/controllers/v0/institutions_controller_spec.rb
+++ spec/controllers/v0/institutions_controller_spec.rb
@@ -338,6 +338,15 @@ RSpec.describe V0::InstitutionsController, type: :controller do
       expect(response).to match_response_schema('institutions')
     end
 
+    it 'search returns results correctly ordered results' do
+      create(:institution, institution: 'HARVARD', gibill: 1, version_id: Version.current_production.id)
+      first = create(:institution, institution: 'HARVARDY', gibill: 100, version_id: Version.current_production.id)
+      get(:index, params: { name: 'HARVARD', fuzzy_search: true })
+      expect(JSON.parse(response.body)['data'][0]['attributes']['name']).to eq(first.institution)
+      expect(response.content_type).to eq('application/json')
+      expect(response).to match_response_schema('institutions')
+    end
+
     it 'search uses fuzzy_search flag' do
       create(:institution, :independent_study, version_id: Version.current_production.id)
       get(:index, params: { name: 'UNIVERSITY OF NDEPENDENT STUDY' })
