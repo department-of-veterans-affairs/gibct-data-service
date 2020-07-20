75086e10c8ead46002e5814bb9c7a59b2ad679ea
Joe <elder_joseph@bah.com>
2020-07-20 15:06:41 -0400

    10985: Revert yarn files

--- app/models/institution_program.rb
+++ app/models/institution_program.rb
@@ -44,16 +44,22 @@ class InstitutionProgram < ApplicationRecord
 
   # Finds exact-matching facility_code or partial-matching school and city names
   #
-  scope :search, lambda { |search_term|
+  scope :search, lambda { |search_term, fuzzy_search = false|
     return if search_term.blank?
 
     clause = [
       'institution_programs.facility_code = (:facility_code)',
-      'institutions.institution LIKE (:upper_search_term)',
-      'lower(description) LIKE (:search_term)',
-      'lower(institutions.physical_city) LIKE (:search_term)',
-      'institutions.physical_zip LIKE (:search_term)'
+      'lower(description) LIKE (:search_term)'
     ]
+
+    if fuzzy_search
+      clause << 'institution % :upper_search_term'
+      clause << 'physical_city % :upper_search_term'
+      clause << 'institutions.physical_zip LIKE (:search_term)'
+    else
+      clause << 'institutions.institution LIKE (:upper_search_term)'
+      clause << 'lower(institutions.physical_city) LIKE (:search_term)'
+    end
 
     where(
       sanitize_sql_for_conditions(
