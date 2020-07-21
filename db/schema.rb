48eeefb063169a073d9d1352b351d8d4cb136d78
hughes-dustin <hughes_dustin@bah.com>
2020-07-21 12:13:33 -0400

    db changes for 10883

--- db/schema.rb
+++ db/schema.rb
@@ -1520,7 +1520,7 @@ ActiveRecord::Schema.define(version: 2020_07_18_131012) do
   end
 
   create_table "va_caution_flags", id: :serial, force: :cascade do |t|
-    t.string "institution_id"
+    t.string "facility_code"
     t.string "institution_name"
     t.string "school_system_name"
     t.string "settlement_title"
