7356bcccad7b8ee43eaa12a132e4b10426b131a3
Matt Roth <roth_matthew@bah.com>
2020-07-24 15:38:18 -0400

    rake ci

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -233,17 +233,15 @@ class Institution < ApplicationRecord
   scope :search, lambda { |search_term, include_address = false, fuzzy_search = false|
     return if search_term.blank?
 
-    clause = ['facility_code = :facility_code']
+    clause = ['facility_code = :facility_code', 'ialias LIKE :upper_search_term']
 
     if fuzzy_search
       clause << 'SIMILARITY(institution, :search_term) > :name_threshold'
       clause << 'SIMILARITY(city, :search_term) > :city_threshold'
       clause << 'zip = :search_term'
-      clause << 'ialias LIKE :upper_search_term'
     else
       clause << 'institution LIKE :upper_search_term'
       clause << 'city LIKE :upper_search_term'
-      clause << 'ialias LIKE :upper_search_term'
     end
 
     if include_address
