ed6983fbca93685162370b6ebd3a9fec7bf22f60
hughes-dustin <hughes_dustin@bah.com>
2020-07-29 13:59:54 -0400

    added date validation for va caution flags

--- app/models/va_caution_flag.rb
+++ app/models/va_caution_flag.rb
@@ -5,14 +5,30 @@ class VaCautionFlag < ApplicationRecord
   CSV_CONVERTER_INFO = {
     'id' => { column: :facility_code, converter: FacilityCodeConverter },
     'instnm' => { column: :institution_name, converter: InstitutionConverter },
-    'school_system_name' => { column: :school_system_name, converter: BaseConverter },
-    'settlement_title' => { column: :settlement_title, converter: BaseConverter },
-    'settlement_description' => { column: :settlement_description, converter: BaseConverter },
-    'settlement_date' => { column: :settlement_date, converter: DateConverter },
-    'settlement_link' => { column: :settlement_link, converter: BaseConverter },
-    'school_closing_date' => { column: :school_closing_date, converter: DateConverter },
-    'sec_702' => { column: :sec_702, converter: BaseConverter }
+    'school system name' => { column: :school_system_name, converter: BaseConverter },
+    'settlement title' => { column: :settlement_title, converter: BaseConverter },
+    'settlement description' => { column: :settlement_description, converter: BaseConverter },
+    'settlement date' => { column: :settlement_date, converter: BaseConverter },
+    'settlement link' => { column: :settlement_link, converter: BaseConverter },
+    'school closing date' => { column: :school_closing_date, converter: BaseConverter },
+    'sec 702' => { column: :sec_702, converter: BooleanConverter }
   }.freeze
 
   validates :facility_code, presence: true
+  validate :validate_date_fields
+
+  private
+
+  def validate_date_fields
+    begin
+      Date.strptime(settlement_date, '%m/%d/%y') if settlement_date
+    rescue ArgumentError
+      errors.add(:settlement_date, 'must be m/d/yy')
+    end
+    begin
+      Date.strptime(school_closing_date, '%m/%d/%y') if school_closing_date
+    rescue ArgumentError
+      errors.add(:school_closing_date, 'must be m/d/yy')
+    end
+  end
 end
