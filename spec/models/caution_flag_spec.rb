f5f2d2c6608ed23fd50f88c88140e45d33da912a
Matt Roth <roth_matthew@bah.com>
2020-08-10 11:16:28 -0400

    cleanup

--- spec/models/caution_flag_spec.rb
+++ spec/models/caution_flag_spec.rb
@@ -20,18 +20,4 @@ RSpec.describe CautionFlag, type: :model do
       expect(described_class.distinct_flags.to_a.size).to eq(1)
     end
   end
-
-  describe 'when mapping' do
-    let(:version) { create :version, :preview }
-
-    it 'sets titles for all rules' do
-      create :caution_flag, :accreditation_issue_pre_map, version_id: version.id
-      create :caution_flag_rule, :accreditation_rule
-
-      described_class.map(version.id)
-
-      expect(described_class.select(:title)
-                 .where(version_id: version.id).pluck(:title)).to all(be)
-    end
-  end
 end
