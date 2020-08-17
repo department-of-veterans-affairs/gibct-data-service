17b1cdaf0cb648762da8ca8eade11ba93edce847
Matt Roth <roth_matthew@bah.com>
2020-08-17 09:51:28 -0400

    Update db/migrate/20200814183045_drop_settlement.rb
    Co-authored-by: Michael R. Fleet <f1337@users.noreply.github.com>

--- db/migrate/20200814183045_drop_settlement.rb
+++ db/migrate/20200814183045_drop_settlement.rb
@@ -1,7 +1,5 @@
 class DropSettlement < ActiveRecord::Migration[5.2]
   def change
-    if table_exists?("settlements")
-      drop_table :settlements
-    end
+    drop_table :settlements, if_exists: true
   end
 end
