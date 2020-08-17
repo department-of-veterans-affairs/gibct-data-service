b94b046f9d6fd71fa7eb472968b7f2b2dde5946a
Matt Roth <roth_matthew@bah.com>
2020-08-17 09:51:56 -0400

    Update db/migrate/20200814183825_drop_rule.rb
    Co-authored-by: Michael R. Fleet <f1337@users.noreply.github.com>

--- db/migrate/20200814183825_drop_rule.rb
+++ db/migrate/20200814183825_drop_rule.rb
@@ -1,7 +1,5 @@
 class DropRule < ActiveRecord::Migration[5.2]
   def change
-    if !table_exists?("caution_flag_rules") && table_exists?("rules")
-      drop_table :rules
-    end
+    drop_table :rules, if_exists: true
   end
 end
