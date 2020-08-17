9ecac9e5063541ba7f24bd7d76e5bc82400403ff
Matt Roth <roth_matthew@bah.com>
2020-08-17 09:51:37 -0400

    Update db/migrate/20200814183406_drop_sec702_school.rb
    Co-authored-by: Michael R. Fleet <f1337@users.noreply.github.com>

--- db/migrate/20200814183406_drop_sec702_school.rb
+++ db/migrate/20200814183406_drop_sec702_school.rb
@@ -1,7 +1,5 @@
 class DropSec702School < ActiveRecord::Migration[5.2]
   def change
-    if table_exists?("sec702_schools")
-      drop_table :sec702_schools
-    end
+    drop_table :sec702_schools, if_exists: true
   end
 end
