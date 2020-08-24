fe39374b04127af40209523e44c2b06b31f62c29
Joe <elder_joseph@bah.com>
2020-08-24 16:15:17 -0400

    Corrections to logic/wording

--- app/validators/va_caution_flag_validator.rb
+++ app/validators/va_caution_flag_validator.rb
@@ -4,9 +4,9 @@ class VaCautionFlagValidator < ActiveModel::Validator
   def validate(record)
     if !record.settlement_title.nil? || !record.settlement_description.nil? || !record.settlement_link.nil? || !record.settlement_date.nil?
       if record.settlement_title.nil? && record.settlement_description.nil?
-        record.errors[:base] << 'Both record.settlement title and record.settlement description are required fo the record.settlement caution flag to be displayed.'
+        record.errors[:base] << 'The row has settlement data, but does not have a Title and Description.'
       elsif record.settlement_title.nil? || record.settlement_description.nil?
-        record.errors[:base] << 'The row has record.settlement data, but does not have a Title and Description.'
+        record.errors[:base] << 'Both settlement title and settlement description are required for the settlement caution flag to be displayed.'
       end
     end
   end
