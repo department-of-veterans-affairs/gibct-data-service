c3eb898de5b5ee48d26ab3b5e50c6da6c0edfba9
Matt Roth <roth_matthew@bah.com>
2020-08-28 15:30:28 -0400

    decrease gibill weight

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -270,7 +270,7 @@ class Institution < ApplicationRecord
                      'CASE WHEN UPPER(institution) = :upper_search_term THEN 1 ELSE 0 END',
                      '(COALESCE(SIMILARITY(institution, :search_term), 0))']
 
-    weighted_sort << '(COALESCE(gibill, 0)/CAST(:max_gibill as FLOAT))' if max_gibill.nonzero?
+    weighted_sort << '((COALESCE(gibill, 0)/CAST(:max_gibill as FLOAT))/3)' if max_gibill.nonzero?
 
     order_by = "#{weighted_sort.join(' + ')} DESC NULLS LAST, institution"
     sanitized_order_by = Institution.sanitize_sql_for_conditions([order_by,
