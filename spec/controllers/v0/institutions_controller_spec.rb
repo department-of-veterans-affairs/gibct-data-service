77a44617d869dd12f441ce87b1cfad606777ae39
Matt Roth <roth_matthew@bah.com>
2020-09-02 13:51:47 -0400

    unit test and threshold change

--- spec/controllers/v0/institutions_controller_spec.rb
+++ spec/controllers/v0/institutions_controller_spec.rb
@@ -326,7 +326,7 @@ RSpec.describe V0::InstitutionsController, type: :controller do
       create(:institution, :independent_study, version_id: Version.current_production.id)
       create(:institution, :uchicago, version_id: Version.current_production.id)
       get(:index, params: { name: 'UNIVERSITY OF NDEPENDENT STUDY', fuzzy_search: true })
-      expect(JSON.parse(response.body)['data'].count).to eq(2)
+      expect(JSON.parse(response.body)['data'].count).to eq(1)
       expect(response.content_type).to eq('application/json')
       expect(response).to match_response_schema('institutions')
     end
@@ -335,7 +335,7 @@ RSpec.describe V0::InstitutionsController, type: :controller do
       first = create(:institution, :independent_study, version_id: Version.current_production.id)
       create(:institution, :uchicago, version_id: Version.current_production.id)
       get(:index, params: { name: 'UNIVERSITY OF INDEPENDENT STUDY', fuzzy_search: true })
-      expect(JSON.parse(response.body)['data'].count).to eq(2)
+      expect(JSON.parse(response.body)['data'].count).to eq(1)
       expect(JSON.parse(response.body)['data'][0]['attributes']['name']).to eq(first.institution)
       expect(response.content_type).to eq('application/json')
       expect(response).to match_response_schema('institutions')
