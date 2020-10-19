462ec98e45bc8eb22081c56cf42afc2969d20c36
Matt Roth <roth_matthew@bah.com>
2020-10-19 15:57:54 -0400

    add common file defaults retrieval

--- lib/common/shared.rb
+++ lib/common/shared.rb
@@ -9,5 +9,20 @@ module Common
     def klass
       name.constantize
     end
+
+    def self.file_type_defaults(klass, options = {})
+      # Pull the default CSV options to be used
+      # If default CSV options exist overwrite generic defaults
+      generic_options = Rails.application.config.csv_defaults['generic']
+      klass_options = Rails.application.config.csv_defaults[klass]
+      default_options = if klass_options.present?
+                          generic_options.merge(klass_options)
+                        else
+                          generic_options
+                        end
+
+      # Merge with provided options
+      default_options.transform_keys(&:to_sym).merge(options)
+    end
   end
 end
