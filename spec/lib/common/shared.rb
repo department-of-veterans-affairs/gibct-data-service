711dfa49062aeffa710f3c141df0150f6c3de398
Matt Roth <roth_matthew@bah.com>
2020-10-22 14:07:13 -0400

    unit tests

---
+++ spec/lib/common/shared.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Common::Shared do
+  context 'klass' do
+    it 'returns class object' do
+      expect(Institution.klass).to eq(Institution)
+    end
+  end
+
+  context 'file_type_default' do
+    it 'returns generic options' do
+      generic_options = Rails.application.config.csv_defaults['generic'].transform_keys(&:to_sym)
+      expect(described_class.file_type_defaults(CalculatorConstant.name))
+          .to eq(generic_options)
+    end
+
+    it 'returns class specific options' do
+      program_options = described_class.file_type_defaults(Program.name)
+      expect(program_options[:col_sep]).to eq('|')
+    end
+
+    it 'returns overridden options' do
+      program_options = described_class.file_type_defaults(Program.name, {skip_lines: 1})
+      expect(program_options[:col_sep]).to eq('|')
+      expect(program_options[:skip_lines]).to eq(1)
+
+    end
+  end
+
+  context 'convert_csv_header' do
+    it 'replaces - with underscores' do
+      header = 'a-header--to--&--test'
+      expect(described_class.convert_csv_header(header)).to eq('a_header_to_&_test')
+    end
+    it 'replaces spaces with underscores' do
+      header = 'a header to  &  test'
+      expect(described_class.convert_csv_header(header)).to eq('a_header_to_&_test')
+    end
+  end
+
+end
---
+++ spec/lib/roo_helper/loader_spec.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe RooHelper::Loader do
+
+  # describe 'header checking' do
+  #   it 'has no missing or extra headers for a normal csv' do
+  #     upload.check_for_headers
+  #
+  #     expect(upload.missing_headers).to be_empty
+  #     expect(upload.extra_headers).to be_empty
+  #   end
+  #
+  #   it 'has missing headers when a csv column is missing' do
+  #     upload = build :upload, csv_name: 'weam_missing_column.csv'
+  #     upload.check_for_headers
+  #
+  #     expect(upload.missing_headers).not_to be_empty
+  #     expect(upload.extra_headers).to be_empty
+  #   end
+  #
+  #   it 'has extra headers when a csv column is added' do
+  #     upload = build :upload, csv_name: 'weam_extra_column.csv'
+  #     upload.check_for_headers
+  #
+  #     expect(upload.missing_headers).to be_empty
+  #     expect(upload.extra_headers).not_to be_empty
+  #   end
+  #
+  #   context 'with insufficient information' do
+  #     it 'has no missing or extra headers if upload_file not valid' do
+  #       upload.upload_file = nil
+  #       upload.check_for_headers
+  #
+  #       expect(upload.missing_headers).to be_empty
+  #       expect(upload.extra_headers).to be_empty
+  #     end
+  #
+  #     it 'has no missing or extra headers if csv_type not valid' do
+  #       upload.csv_type = nil
+  #       upload.check_for_headers
+  #
+  #       expect(upload.missing_headers).to be_empty
+  #       expect(upload.extra_headers).to be_empty
+  #     end
+  #
+  #     it 'has no missing or extra headers if skip_lines is not valid' do
+  #       upload.skip_lines = nil
+  #       upload.check_for_headers
+  #
+  #       expect(upload.missing_headers).to be_empty
+  #       expect(upload.extra_headers).to be_empty
+  #     end
+  #   end
+  # end
+
+  # describe 'set_col_sep' do
+  #   it 'sets col_sep to comma when csv first line' do
+  #     first_line = 'a,b,c'
+  #     upload = build :upload
+  #     upload.send(:set_col_sep, first_line)
+  #     expect(upload.col_sep).to eq(',')
+  #   end
+  #
+  #   it 'sets col_sep to pipe when pipe delimited first line and contains a comma in a column' do
+  #     first_line = 'a|,b|c'
+  #     upload = create :upload
+  #     upload.send(:set_col_sep, first_line)
+  #     expect(upload.col_sep).to eq('|')
+  #   end
+  #
+  #   it 'raises error when neither comma or pipe are found' do
+  #     first_line = 'a/b\c'
+  #     upload = create :upload
+  #     error_message = 'Unable to determine column separators, valid separators equal "|" and ","'
+  #     expect { upload.send(:set_col_sep, first_line) }.to raise_error(StandardError, error_message)
+  #   end
+  # end
+end
