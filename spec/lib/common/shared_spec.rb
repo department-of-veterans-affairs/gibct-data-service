cc5fefaadc2739a74f77f3a53ffb37193bd34f4d
Matt Roth <roth_matthew@bah.com>
2020-10-23 11:43:17 -0400

    rake ci

--- spec/lib/common/shared_spec.rb
+++ spec/lib/common/shared_spec.rb
@@ -3,17 +3,17 @@
 require 'rails_helper'
 
 describe Common::Shared do
-  context 'klass' do
+  describe 'klass' do
     it 'returns class object' do
       expect(Institution.klass).to eq(Institution)
     end
   end
 
-  context 'file_type_default' do
+  describe 'file_type_default' do
     it 'returns generic options' do
       generic_options = Rails.application.config.csv_defaults['generic'].transform_keys(&:to_sym)
       expect(described_class.file_type_defaults(CalculatorConstant.name))
-          .to eq(generic_options)
+        .to eq(generic_options)
     end
 
     it 'returns class specific options' do
@@ -22,22 +22,21 @@ describe Common::Shared do
     end
 
     it 'returns overridden options' do
-      program_options = described_class.file_type_defaults(Program.name, {skip_lines: 1})
+      program_options = described_class.file_type_defaults(Program.name, { skip_lines: 1 })
       expect(program_options[:col_sep]).to eq('|')
       expect(program_options[:skip_lines]).to eq(1)
-
     end
   end
 
-  context 'convert_csv_header' do
+  describe 'convert_csv_header' do
     it 'replaces - with underscores' do
       header = 'a-header--to--&--test'
       expect(described_class.convert_csv_header(header)).to eq('a_header_to_&_test')
     end
+
     it 'replaces spaces with underscores' do
       header = 'a header to  &  test'
       expect(described_class.convert_csv_header(header)).to eq('a_header_to_&_test')
     end
   end
-
 end
--- spec/lib/roo_helper/loader_spec.rb
+++ spec/lib/roo_helper/loader_spec.rb
@@ -6,18 +6,20 @@ describe RooHelper::Loader do
   let(:edu_program) { EduProgram }
   let(:program) { Program }
 
-  context 'converter_info' do
-    let(:key) { 'facility_code'}
+  describe 'converter_info' do
+    let(:key) { 'facility_code' }
+
     it 'returns correct info object for a header with underscores' do
       info = edu_program.send(:converter_info, edu_program, key)
       expect(info).to eq(edu_program::CSV_CONVERTER_INFO[key])
     end
+
     it 'returns correct info object for a header with spaces' do
       file_header = 'facility code'
       info = edu_program.send(:converter_info, edu_program, file_header)
       expect(info).to eq(edu_program::CSV_CONVERTER_INFO[key])
-
     end
+
     it 'returns correct info object for a header with dashes' do
       file_header = 'facility-code'
       info = edu_program.send(:converter_info, edu_program, file_header)
@@ -25,16 +27,16 @@ describe RooHelper::Loader do
     end
   end
 
-  context 'header checking' do
+  describe 'header_warnings' do
     it 'has no missing or extra headers for a normal csv' do
-      file_headers = edu_program::CSV_CONVERTER_INFO.keys.map{|k| k.gsub('_', ' ')}
+      file_headers = edu_program::CSV_CONVERTER_INFO.keys.map { |k| k.gsub('_', ' ') }
       header_warnings = edu_program.send(:header_warnings, edu_program, file_headers)
 
       expect(header_warnings).to be_empty
     end
 
     it 'has missing headers when a csv column is missing' do
-      file_headers = edu_program::CSV_CONVERTER_INFO.keys.map{|k| k.gsub('_', ' ')}
+      file_headers = edu_program::CSV_CONVERTER_INFO.keys.map { |k| k.gsub('_', ' ') }
       missing_header = file_headers.pop
       header_warnings = edu_program.send(:header_warnings, edu_program, file_headers)
 
@@ -45,7 +47,7 @@ describe RooHelper::Loader do
     it 'has extra headers when a csv column is added' do
       extra_header = 'extra-header'
       file_headers = edu_program::CSV_CONVERTER_INFO.keys
-                         .map{|k| k.gsub('_', ' ')}.push(extra_header)
+                                                    .map { |k| k.gsub('_', ' ') }.push(extra_header)
       header_warnings = edu_program.send(:header_warnings, edu_program, file_headers)
 
       extra_warning = "#{extra_header.gsub(/\s+|-+/, ' ').capitalize} is an extra header"
@@ -54,16 +56,16 @@ describe RooHelper::Loader do
     end
   end
 
-  context 'merge_options' do
+  describe 'merge_options' do
     it 'sets defaults' do
       file_options = {}
       merged_options = edu_program.send(:merge_options, file_options)
 
       expect(merged_options[:parse_as_xml]).to be_falsey
-      sheet_1 = merged_options[:sheets][0]
-      expect(sheet_1[:skip_lines]).to eq(0)
-      expect(sheet_1[:first_line]).to eq(2)
-      expect(sheet_1[:klass]).to eq(edu_program)
+      sheet1 = merged_options[:sheets][0]
+      expect(sheet1[:skip_lines]).to eq(0)
+      expect(sheet1[:first_line]).to eq(2)
+      expect(sheet1[:klass]).to eq(edu_program)
     end
 
     it 'does not override parse_as_xml' do
@@ -74,48 +76,48 @@ describe RooHelper::Loader do
     end
 
     it 'does not override sheets objects' do
-      file_options = {sheets: [{klass: program, skip_lines: 2, first_line: 3}]}
+      file_options = { sheets: [{ klass: program, skip_lines: 2, first_line: 3 }] }
       merged_options = program.send(:merge_options, file_options)
 
-      sheet_1 = merged_options[:sheets][0]
-      expect(sheet_1[:skip_lines]).to eq(2)
-      expect(sheet_1[:first_line]).to eq(3)
-      expect(sheet_1[:klass]).to eq(program)
+      sheet1 = merged_options[:sheets][0]
+      expect(sheet1[:skip_lines]).to eq(2)
+      expect(sheet1[:first_line]).to eq(3)
+      expect(sheet1[:klass]).to eq(program)
     end
 
     it 'sets default sheets array object properties' do
-      file_options = {sheets: [{klass: program}]}
+      file_options = { sheets: [{ klass: program }] }
       merged_options = program.send(:merge_options, file_options)
 
-      sheet_1 = merged_options[:sheets][0]
-      expect(sheet_1[:skip_lines]).to eq(0)
-      expect(sheet_1[:first_line]).to eq(2)
-      expect(sheet_1[:klass]).to eq(program)
+      sheet1 = merged_options[:sheets][0]
+      expect(sheet1[:skip_lines]).to eq(0)
+      expect(sheet1[:first_line]).to eq(2)
+      expect(sheet1[:klass]).to eq(program)
     end
   end
 
   describe 'csv_col_sep' do
     it 'returns , for comma separated values' do
       csv_file = "./spec/fixtures/#{edu_program.name.underscore}.csv"
-      file_options = {sheets: [{klass: edu_program}]}
+      file_options = { sheets: [{ klass: edu_program }] }
 
       expect(edu_program.send(:csv_col_sep, csv_file, file_options)).to eq(',')
     end
 
     it 'returns | for pipe delimited file' do
       csv_file = "./spec/fixtures/#{program.name.underscore}.csv"
-      file_options = {sheets: [{klass: program}]}
+      file_options = { sheets: [{ klass: program }] }
 
       expect(program.send(:csv_col_sep, csv_file, file_options)).to eq('|')
     end
 
     it 'raises error when neither comma or pipe are found' do
-      csv_file = "./spec/fixtures/invalid_col_sep.csv"
-      file_options = {sheets: [{klass: edu_program, skip_lines: 0}]}
+      csv_file = './spec/fixtures/invalid_col_sep.csv'
+      file_options = { sheets: [{ klass: edu_program, skip_lines: 0 }] }
 
       error_message = 'Unable to determine column separators, valid separators equal "|" and ","'
       expect { edu_program.send(:csv_col_sep, csv_file, file_options) }
-          .to raise_error(StandardError, error_message)
+        .to raise_error(StandardError, error_message)
     end
   end
 end
