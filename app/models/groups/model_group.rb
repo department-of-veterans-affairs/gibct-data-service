f6b0f4f9dcc87f42d613a6c0cc4d027e62eb16f2
Matt Roth <roth_matthew@bah.com>
2020-10-26 15:19:05 -0400

    Delete model_group.rb

--- app/models/groups/model_group.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This should not have to be here but ruby is not loading this in config/initializers/roo_helper.rb
-Dir["#{Rails.application.config.root}/lib/roo_helper/**/*.rb"].sort.each { |f| require(f) }
-
-class ModelGroup
-  include RooHelper
-
-end
