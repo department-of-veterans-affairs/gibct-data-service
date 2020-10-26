9f04c99b37003b5edbdd46e5499bbf7239e5a890
Devin McCurdy <mccurdy_devin@bah.com>
2020-10-26 13:42:10 -0400

    docker test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,14 +9,14 @@ apt-get install nodejs
 apt-get install npm
 npm install yarn -g
 
-## Configure gibct application
-#RUN mkdir -p /src/gibct && chown gibct:gibct /src/gibct
-#VOLUME /src/gibct
-#WORKDIR /src/gibct
-#
-#ADD . /src/gibct
-#RUN ["/bin/bash", "--login", "-c", "bundle install -j4"]
-#RUN yarn install --force --non-interactive
-#yarn install --force --non-interactive
+# Configure gibct application
+RUN mkdir -p /src/gibct && chown gibct:gibct /src/gibct
+VOLUME /src/gibct
+WORKDIR /src/gibct
+
+ADD . /src/gibct
+RUN ["/bin/bash", "--login", "-c", "bundle install -j4"]
+RUN yarn install --force --non-interactive
+yarn install --force --non-interactive
 
 exec "$@"
