df10695eb96d24ec8838c35e5a7654c0633368ee
Matt Roth <roth_matthew@bah.com>
2020-10-28 12:02:50 -0400

    Refactor CSV type to be Upload types (#753)
    * create more generic upload_types
    
    * rake ci
    
    * unit tests
    
    * remove method that was available throughout app
    
    * rake ci
    
    * spec cleanup

---
+++ config/initializers/upload_types.rb
@@ -0,0 +1,21 @@
+UPLOAD_TYPES = [
+    *CSV_TYPES_TABLES
+].freeze
+
+UPLOAD_TYPES_ALL_NAMES = UPLOAD_TYPES.map do |upload|
+  klass = upload[:klass]
+  return klass if klass.is_a? String
+  klass.name
+end.freeze
+
+UPLOAD_TYPES_REQUIRED_NAMES = UPLOAD_TYPES.select { |upload| upload[:required?] }.map do |upload|
+  klass = upload[:klass]
+  return klass if klass.is_a? String
+  klass.name
+end.freeze
+
+UPLOAD_TYPES_NO_PROD_NAMES = UPLOAD_TYPES.select { |upload| upload[:not_prod_ready?] }.map do |upload|
+  klass = upload[:klass]
+  return klass if klass.is_a? String
+  klass.name
+end.freeze
