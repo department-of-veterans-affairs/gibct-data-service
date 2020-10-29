88399720cc88aba811873176766a9c56fbae6bdd
Matt Roth <roth_matthew@bah.com>
2020-10-29 08:17:59 -0400

    GIDS: Add the ability to upload the accreditation excel file #13863 (#751)
    * add roo gems
    
    * sample csv data
    
    * Update settings.yml
    
    * move valid upload types to settings
    
    * move common loading code out of csv_helper/loader
    
    * mvoe shared out
    
    * rename load to load_from_csv, load_from_api to load
    
    * excel helper and available file types
    
    * revert where values come from
    
    * move includes CsvHelper to ApplicationRecord
    
    * lint rolling
    
    * Update settings.yml
    
    * make all ApplicationRecords include CsvHelper
    
    * rename load and load_from_api to load_from_csv and load respectively
    
    * Update shared_examples_for_loadable.rb
    
    * make and use ImportableRecord
    
    * Update importable_record.rb
    
    * add logic in uploads_controller for who it's going to call
    
    * load csv or excel logic and cleanup
    
    * got the file from https://ope.ed.gov/dapip/#/download-data-files to work
    
    * cleanupo
    
    * lint
    
    * rename from excel to roo
    
    * get a good xlsx working
    
    * Update loader.rb
    
    * lint
    
    * xls and csv testing
    
    * better way to handle the bad xlsx, make sure when using it to set the option to true
    
    * Update uploads_controller.rb
    
    * skip lines, liberal parsing, tweak code structure, fixes and other tweaks
    
    * lint
    
    * split file options and sheet options
    
    * Update loader.rb
    
    * rake db:seed testing
    
    * have errors be on top when uploading
    
    * add common file defaults retrieval
    
    * cleanup
    
    * switch csv_converter_info keys to not use spaces or dashes
    
    * change how csv file defaults are pulled, removed ones that are duplicates of generic, the unit tests pass
    
    * Update seed_utils.rb
    
    * replace underscores in exports with spaces for column headers
    
    * unit tests
    
    * Update loader_spec.rb
    
    * replace removed unit tests, removed commented out code
    
    * rake ci
    
    * cleanup
    
    * cleanup
    
    * Update uploads_controller.rb
    
    * create model groups and accreditation
    
    * group controller
    
    * setup
    
    * more
    
    * create more generic upload_types
    
    * rake ci
    
    * Update upload_types.rb
    
    * from_group_type
    
    * tangent
    
    * refactor info box
    
    * mvc
    
    * revert
    
    * Delete model_group.rb
    
    * unit test
    
    * requirements displays for each type in a group, redirect index to uploads index
    
    * new upload page works correctly, error messages display correct, new page is reset after an error correctly, and then it fails on upload
    
    * cleanup
    
    * clenaup
    
    * uploads work
    
    * success and warning messages
    
    * validate and error messages
    
    * Update group.rb
    
    * cleanup
    
    * fix issue
    
    * allow for parse as xml to be changed if an error occurs
    
    * cleanup

---
+++ app/models/group.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Group < Upload
+  include RooHelper
+
+  attr_accessor :sheet_type_list, :group_config, :parse_as_xml
+
+  def initialize(attributes = nil)
+    super(attributes)
+    self.group_config = Group.group_config_options(csv_type)
+  end
+
+  def self.create_from_group_type(group_type)
+    group = Group.new(group_type)
+    group.parse_as_xml = group.group_config[:parse_as_xml?]
+    group
+  end
+
+  def self.group_config_options(group_type)
+    GROUP_FILE_TYPES.select { |g| g[:klass] == group_type }.first
+  end
+
+  def sheet_names
+    @sheet_names ||= group_config[:types].map(&:name)
+  end
+
+  def sheets
+    @sheets ||= group_config[:types]
+  end
+end
