e990bf214f093cfd0aa26aa3bfe03f0a3f88c5a8
Matt Roth <roth_matthew@bah.com>
2020-11-06 11:26:33 -0500

    Update group_types.rb

--- config/initializers/group_types.rb
+++ config/initializers/group_types.rb
@@ -11,4 +11,3 @@ GROUP_FILE_TYPES = [
 ].freeze
 
 GROUP_FILE_TYPES_NAMES = GROUP_FILE_TYPES.map { |g| g[:klass] }.freeze
-GROUP_PARSE_AS_XML = GROUP_FILE_TYPES.select { |g| g[:parse_as_xml?].present? }.map { |g| g[:klass] }.freeze
