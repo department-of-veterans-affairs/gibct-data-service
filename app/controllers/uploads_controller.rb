d975932f64025263babb97911b33e79501a9f04d
Matt Roth <roth_matthew@bah.com>
2020-11-17 09:40:35 -0500

    remove requires

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-#
-# This should not have to be here but ruby is not loading this in config/initializers/roo_helper.rb
-Dir["#{Rails.application.config.root}/lib/roo_helper/**/*.rb"].sort.each { |f| require(f) }
-
 class UploadsController < ApplicationController
   def index
     @uploads = Upload.paginate(page: params[:page]).order(created_at: :desc)
