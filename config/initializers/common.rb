87bebb7e529a1ce9c80f82f1cf7f30b569b8e858
Matt Roth <roth_matthew@bah.com>
2020-11-17 13:39:27 -0500

    Update common.rb

--- config/initializers/common.rb
+++ config/initializers/common.rb
@@ -1,7 +1,4 @@
 require 'common/exceptions'
-require 'common/exporter'
-require 'common/loader'
-require 'common/shared'
 
 Rails.application.config.after_initialize do
   deployment_env = ENV.fetch('DEPLOYMENT_ENV')
