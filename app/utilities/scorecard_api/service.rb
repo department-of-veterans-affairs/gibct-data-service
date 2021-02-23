4186ae81410f7eb6495c32ff41155ef69c6897f0
Joe <elder_joseph@bah.com>
2021-02-23 14:32:41 -0500

    19571: Correct linting

--- app/utilities/scorecard_api/service.rb
+++ app/utilities/scorecard_api/service.rb
@@ -108,18 +108,18 @@ module ScorecardApi
       degree_program_results = []
       results.each do |result|
         next unless result.key?(:'latest.programs.cip_4_digit')
-          result[:'latest.programs.cip_4_digit'].each do |degree_program|
-            scorecard_degree_program = ScorecardDegreeProgram.new
-            scorecard_degree_program[:unitid] = degree_program[:unit_id]
-            scorecard_degree_program[:ope6_id] = degree_program[:ope6_id]
-            scorecard_degree_program[:control] = degree_program[:school][:type]
-            scorecard_degree_program[:main] = degree_program[:school][:main_campus]
-            scorecard_degree_program[:cip_code] = degree_program[:code]
-            scorecard_degree_program[:cip_desc] = degree_program[:title]
-            scorecard_degree_program[:cred_lev] = degree_program[:credential][:level]
-            scorecard_degree_program[:cred_desc] = degree_program[:credential][:title]
-            degree_program_results.push(scorecard_degree_program)
 
+        result[:'latest.programs.cip_4_digit'].each do |degree_program|
+          scorecard_degree_program = ScorecardDegreeProgram.new
+          scorecard_degree_program[:unitid] = degree_program[:unit_id]
+          scorecard_degree_program[:ope6_id] = degree_program[:ope6_id]
+          scorecard_degree_program[:control] = degree_program[:school][:type]
+          scorecard_degree_program[:main] = degree_program[:school][:main_campus]
+          scorecard_degree_program[:cip_code] = degree_program[:code]
+          scorecard_degree_program[:cip_desc] = degree_program[:title]
+          scorecard_degree_program[:cred_lev] = degree_program[:credential][:level]
+          scorecard_degree_program[:cred_desc] = degree_program[:credential][:title]
+          degree_program_results.push(scorecard_degree_program)
         end
       end
       degree_program_results
