f5497edc005766f17edc6b07eac9d9dc4fc556fd
Matt Roth <roth_matthew@bah.com>
2021-05-03 11:15:10 -0400

    refactor

---
+++ app/utilities/institution_builder/scorecard_builder.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module InstitutionBuilder
+  class ScorecardBuilder
+    extend Common
+
+    def self.build(version_id)
+      str = <<-SQL
+      UPDATE institutions SET #{columns_for_update(Scorecard)}, ialias = scorecards.alias
+      FROM scorecards
+      WHERE institutions.cross = scorecards.cross
+      AND institutions.version_id = #{version_id}
+      SQL
+
+      Institution.connection.update(str)
+    end
+
+    def self.add_lat_lon_from_scorecard(version_id)
+      str = <<-SQL
+      UPDATE institutions SET latitude = scorecards.latitude, longitude = scorecards.longitude
+      FROM scorecards
+      WHERE institutions.cross = scorecards.cross
+      AND institutions.version_id = #{version_id}
+      AND institutions.latitude is NULL
+      SQL
+
+      Institution.connection.update(str)
+    end
+  end
+end
