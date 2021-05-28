bcaace52c0659575d704877bce7d918505375b0e
Matt Roth <roth_matthew@bah.com>
2021-05-28 11:39:40 -0400

    cereal

--- app/serializers/institution_compare_serializer.rb
+++ app/serializers/institution_compare_serializer.rb
@@ -4,12 +4,33 @@
 class InstitutionCompareSerializer < ActiveModel::Serializer
   attribute :institution, key: :name
   attribute :facility_code
+  attribute :physical_city, key: :city
+  attribute :physical_state, key: :state
+  attribute :physical_country, key: :country
+  attribute :accreditation_type
+  attribute :gibill, key: :student_count
+  attribute :rating_average
+  attribute :rating_count
   attribute :institution_type_name, key: :type
-  attribute :city
-  attribute :state
-  attribute :zip
-  attribute :country
+  attribute :caution_flags
+  attribute :caution_flag
+  attribute :student_veteran
+  attribute :yr
+  attribute :campus_type
+  attribute :highest_degree
+  attribute :hbcu
+  attribute :menonly
+  attribute :womenonly
+  attribute :relaffil
+  attribute :preferred_provider
+  attribute :dod_bah
   attribute :bah
+  attribute :latitude
+  attribute :longitude
+  attribute :distance
+  attribute :accredited
+  attribute :vet_tec_provider
+
   attribute :cross
   attribute :flight
   attribute :correspondence
@@ -18,15 +39,11 @@ class InstitutionCompareSerializer < ActiveModel::Serializer
   attribute :f1sysnam, key: :school_system_name
   attribute :f1syscod, key: :school_system_code
   attribute :ialias, key: :alias
-  attribute :highest_degree
   attribute :locale_type
   attribute :address_1
   attribute :address_2
   attribute :address_3
-  attribute :gibill, key: :student_count
   attribute :undergrad_enrollment
-  attribute :yr
-  attribute :student_veteran
   attribute :student_veteran_link
   attribute :poe
   attribute :eight_keys
@@ -61,12 +78,7 @@ class InstitutionCompareSerializer < ActiveModel::Serializer
   attribute :p911_recipients
   attribute :p911_yellow_ribbon
   attribute :p911_yr_recipients
-  attribute :accredited
-  attribute :accreditation_type
   attribute :accreditation_status
-  attribute :caution_flag
-  attribute :caution_flag_reason
-  attribute :caution_flags
   attribute(:complaints) { object.complaints }
   attribute :school_closing
   attribute :school_closing_on
@@ -76,30 +88,16 @@ class InstitutionCompareSerializer < ActiveModel::Serializer
   attribute :priority_enrollment
   attribute :created_at
   attribute :updated_at
-  attribute :physical_address_1
-  attribute :physical_address_2
-  attribute :physical_address_3
-  attribute :physical_city
-  attribute :physical_state
-  attribute :physical_country
   attribute :online_only
   attribute :distance_learning
-  attribute :dod_bah
-  attribute :physical_zip
+  attribute :physical_zip, key: :zip
   attribute :parent_facility_code_id
-  attribute :campus_type
-  attribute :vet_tec_provider
-  attribute :preferred_provider
   attribute :stem_indicator
   attribute :section_103_message
-  attribute :hbcu
   attribute :hcm2
-  attribute :menonly
   attribute :pctfloan
-  attribute :relaffil
-  attribute :womenonly
+  attribute :institution_category_ratings
 
-  link(:self) { v0_institution_url(object.facility_code) }
   def yellow_ribbon_programs
     object.yellow_ribbon_programs.map do |yrp|
       YellowRibbonProgramSerializer.new(yrp)
--- app/serializers/institution_search_result_serializer.rb
+++ app/serializers/institution_search_result_serializer.rb
@@ -30,6 +30,37 @@ class InstitutionSearchResultSerializer < ActiveModel::Serializer
   attribute :accredited
   attribute :vet_tec_provider
 
+  # a. Accreditation [Institution Summary]
+  # c. Length of program [Header]
+  # d. Type of school [Header]
+  # e. Institution locale [Header]
+  # f. Size of school [Header]
+  # g. School mission [Used in search results - Not displayed]
+  # h. Tuition and fees [EYB]
+  # i. GI Bill pays to school [EYB]
+  # j. Out of pocket tuition [EYB]
+  # k. Housing allowance [EYB]
+  # l. Book stipend [EYB]
+  # m. Overall rating [Ratings - behind feature flag]
+  # n. # of veteran ratings [Ratings - behind feature flag]
+  # o. Overall experience Rating [Ratings - behind feature flag]
+  # p. Quality of classes Rating [Ratings - behind feature flag]
+  # q. Online instruction Rating [Ratings - behind feature flag]
+  # r. Job preparation Rating [Ratings - behind feature flag]
+  # s. GI bill support Rating [Ratings - behind feature flag]
+  # t. Veteran community Rating [Ratings - behind feature flag]
+  # u. True to expectations Rating [Ratings - behind feature flag]
+  # w. Student complaints [Cautionary information]
+  # x. Length of VET TEC programs [VETTEC profile]
+  # y. Credit for military training [Additional information]
+  # z. Single point of contact [Additional information]
+  # aa. Yellow Ribbon [Veteran Programs]
+  # ab. Student Veteran Group [Veteran Programs]
+  # ac. Principle of Excellence [Veteran Programs]
+  # ad. 8 Keys to Veteran Success [Veteran Programs]
+  # ae. Military Tuition Assistance (TA) [Veteran Programs]
+  # af. Priority Enrollment [Veteran Programs]
+
   def caution_flags
     object.caution_flags.map do |flag|
       CautionFlagSerializer.new(flag)
