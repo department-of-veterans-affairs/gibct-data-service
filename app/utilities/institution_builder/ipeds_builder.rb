ea675681444a7105549e5083dc095f258b00bed6
Matt Roth <roth_matthew@bah.com>
2021-06-11 14:06:29 -0400

    exclude bad lat/long values

---
+++ app/utilities/institution_builder/ipeds_builder.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module InstitutionBuilder
+  class IpedsBuilder
+    extend Common
+
+    def self.build_ic(version_id)
+      str = <<-SQL
+        institutions.cross = ipeds_ics.cross
+      SQL
+
+      add_columns_for_update(version_id, IpedsIc, str)
+    end
+
+    def self.build_hd(version_id)
+      str = <<-SQL
+        UPDATE institutions SET #{columns_for_update(IpedsHd)}, longitude = ipeds_hds.longitud
+        FROM ipeds_hds
+        WHERE institutions.cross = ipeds_hds.cross
+        AND ipeds_hds.latitude BETWEEN -90 AND 90
+        AND ipeds_hds.longitud BETWEEN -180 AND 180
+        AND institutions.version_id = #{version_id}
+      SQL
+
+      Institution.connection.update(str)
+    end
+
+    def self.build_ic_ay(version_id)
+      str = <<-SQL
+        institutions.cross = ipeds_ic_ays.cross
+      SQL
+
+      add_columns_for_update(version_id, IpedsIcAy, str)
+    end
+
+    def self.build_ic_py(version_id)
+      columns = IpedsIcPy::COLS_USED_IN_INSTITUTION.map(&:to_s).map do |col|
+        %("#{col}" = CASE WHEN institutions.#{col} IS NULL THEN ipeds_ic_pies.#{col} ELSE institutions.#{col} END)
+      end.join(', ')
+
+      str = <<-SQL
+        UPDATE institutions SET #{columns}
+        FROM ipeds_ic_pies
+        WHERE institutions.cross = ipeds_ic_pies.cross
+        AND institutions.version_id = #{version_id}
+      SQL
+
+      Institution.connection.update(str)
+    end
+  end
+end
