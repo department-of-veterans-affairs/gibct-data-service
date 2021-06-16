431c93a9f29fad460fed995d6729201cf91a15e7
Joe <elder_joseph@bah.com>
2021-06-16 10:32:18 -0400

    25818: Initial commit

---
+++ spec/factories/in_state_tuition_policy_url.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :in_state_tuition_policy_url do
+    facility_code { generate :facility_code }
+    in_state_tuition_information { "Contact the School Certifying Official (SCO) for requirements" }
+  end
+end
---
+++ spec/models/in_state_tuition_policy_url_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe InStateTuitionPolicyUrl, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
