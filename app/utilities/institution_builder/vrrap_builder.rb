e3c3a1a2d1ad213c5d26165f42562fd7638f742f
Matt Roth <roth_matthew@bah.com>
2021-06-16 13:38:58 -0400

    cleanup

--- app/utilities/institution_builder/vrrap_builder.rb
+++ app/utilities/institution_builder/vrrap_builder.rb
@@ -6,11 +6,11 @@ module InstitutionBuilder
 
     def self.build(version_id)
       str = <<-SQL
-      UPDATE institutions SET vrrap = true
-      FROM vrrap_providers
-      WHERE institutions.facility_code = vrrap_providers.facility_code
-      AND vaco IS TRUE
-      AND institutions.version_id = #{version_id}
+        UPDATE institutions SET vrrap = true
+        FROM vrrap_providers
+        WHERE institutions.facility_code = vrrap_providers.facility_code
+        AND vaco IS TRUE
+        AND institutions.version_id = #{version_id}
       SQL
 
       Institution.connection.update(str)
