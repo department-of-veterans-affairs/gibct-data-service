fbefa86692cb403f2f3454d8e824784943511242
Joe <elder_joseph@bah.com>
2021-06-16 12:21:25 -0400

    25818: update specs

--- spec/models/in_state_tuition_policy_url_spec.rb
+++ spec/models/in_state_tuition_policy_url_spec.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+
 require 'rails_helper'
+require 'models/shared_examples/shared_examples_for_loadable'
+require 'models/shared_examples/shared_examples_for_exportable'
 
 RSpec.describe InStateTuitionPolicyUrl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like 'a loadable model', skip_lines: 0
+  it_behaves_like 'an exportable model', skip_lines: 0
+
+  describe 'when validating' do
+    it 'has a valid factory' do
+      expect(build(:in_state_tuition_policy_url)).to be_valid
+    end
+  end
 end
