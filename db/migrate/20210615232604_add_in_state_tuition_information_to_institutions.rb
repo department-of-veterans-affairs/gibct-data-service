53b7437b0971ff028a3ddca7110ca4624156e45d
Joe <elder_joseph@bah.com>
2021-06-16 16:09:36 -0400

    25818: update migrations

--- db/migrate/20210615232604_add_in_state_tuition_information_to_institutions.rb
+++ db/migrate/20210615232604_add_in_state_tuition_information_to_institutions.rb
@@ -1,5 +1,6 @@
 class AddInStateTuitionInformationToInstitutions < ActiveRecord::Migration[6.0]
   def change
     add_column :institutions, :in_state_tuition_information, :string
+    add_column :institutions_archives, :in_state_tuition_information, :string
   end
 end
