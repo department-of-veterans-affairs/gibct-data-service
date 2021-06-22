3ae3fdfa1a172a738637df77bb64dd8e15d84bd4
Joe <elder_joseph@bah.com>
2021-06-22 11:36:18 -0400

    25818: Add in_state_tuition_information to institution_profile_serializer

--- app/serializers/institution_profile_serializer.rb
+++ app/serializers/institution_profile_serializer.rb
@@ -105,6 +105,7 @@ class InstitutionProfileSerializer < ActiveModel::Serializer
   attribute :institution_category_ratings
   attribute :rating_average
   attribute :rating_count
+  attribute :in_state_tuition_information
 
   link(:website) { object.website_link }
   link(:scorecard) { object.scorecard_link }
