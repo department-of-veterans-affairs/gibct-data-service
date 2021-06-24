241f35bf8b58c4b483ba17232c960ee6b0d8e9d8
Matt Roth <roth_matthew@bah.com>
2021-06-24 10:31:53 -0400

    add latlong issues to dashboard

---
+++ app/controllers/latitude_longitude_issues_controller.rb
@@ -0,0 +1,14 @@
+class LatitudeLongitudeIssuesController < ApplicationController
+
+  def export
+    flash.alert = "Not implemented"
+
+    redirect_to dashboards_path
+  end
+
+  def import
+    flash.alert = "Not implemented"
+
+    redirect_to dashboards_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,12 @@ Rails.application.routes.draw do
 
   get '/archives' => 'archives#index'
   get '/archives/export/:csv_type/:number' => 'archives#export', as: :archives_export, defaults: { format: 'csv' }
+
   get '/calculator_constants' => 'calculator_constants#index'
   post '/calculator_constants' => 'calculator_constants#update', as: :calculator_constants_update
+
+  get '/latitude_longitude_issues' => 'latitude_longitude_issues#export', as: :latitude_longitude_issues_export
+  get '/latitude_longitude_issues' => 'latitude_longitude_issues#import', as: :latitude_longitude_issues_import
 
   resources :storages, only: [:index, :edit, :update, :show] do
     get 'download' => 'storages#download', on: :member, defaults: { format: 'csv' }
