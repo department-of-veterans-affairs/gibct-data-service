ffa278a2d4e817d12a6e5fda6398d2638c80b630
Matt Roth <roth_matthew@bah.com>
2021-06-29 14:25:23 -0400

    lint

--- spec/models/census_lat_long_spec.rb
+++ spec/models/census_lat_long_spec.rb
@@ -3,8 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe CensusLatLong, type: :model do
-
-  describe "export" do
+  describe 'export' do
     it 'returns binary_data' do
       create :version
       create(:institution, :physical_address, version: Version.latest)
@@ -14,9 +13,12 @@ RSpec.describe CensusLatLong, type: :model do
     end
   end
 
-  describe "add_institution_addresses" do
-    it "includes an institution missing lat long" do
+  describe 'add_institution_addresses' do
+    before do
       create :version
+    end
+
+    it 'includes an institution missing lat long' do
       create(:institution, :physical_address, version: Version.latest)
       create(:institution, :lat_long, version: Version.latest)
 
@@ -25,8 +27,7 @@ RSpec.describe CensusLatLong, type: :model do
       expect(addresses.count).to eq(1)
     end
 
-    it "does not include an institution missing lat long whose facility code also qualifies from weams" do
-      create :version
+    it 'does not include an institution missing lat long whose facility code also qualifies from weams' do
       weam = create(:weam)
       create(:institution, facility_code: weam.facility_code, version: Version.latest)
 
@@ -35,49 +36,48 @@ RSpec.describe CensusLatLong, type: :model do
       expect(addresses.count).to eq(0)
     end
 
-    it "includes an institution missing lat long but uses mailing address" do
-      create :version
+    it 'includes an institution missing lat long but uses mailing address' do
       institution = create(:institution, :mailing_address, version: Version.latest)
       create(:institution, :lat_long, version: Version.latest)
 
-      mailing_values = [institution.facility_code, institution.address, institution.city, institution.state, institution.zip]
+      mailing_values = [institution.facility_code, institution.address, institution.city,
+                        institution.state, institution.zip]
 
       addresses = []
       described_class.add_institution_addresses(addresses, [])
       expect(addresses.count).to eq(1)
       expect(addresses[0]).to eq(mailing_values)
     end
-
   end
 
-  describe "add_weams_physical_addresses" do
-    it "includes a weam row with physical address values" do
+  describe 'add_weams_physical_addresses' do
+    it 'includes a weam row with physical address values' do
       weam = create(:weam, :no_physical_address)
-      weam_1 = create(:weam, :physical_address)
+      weam1 = create(:weam, :physical_address)
 
       addresses = []
-      described_class.add_weams_physical_addresses(addresses, [weam_1, weam])
+      described_class.add_weams_physical_addresses(addresses, [weam1, weam])
       expect(addresses.count).to eq(1)
     end
   end
 
-  describe "add_weams_mailing_addresses" do
-    it "includes a weam row without physical address values" do
+  describe 'add_weams_mailing_addresses' do
+    it 'includes a weam row without physical address values' do
       weam = create(:weam, :mailing_address)
-      weam_1 = create(:weam, :no_mailing_address)
+      weam1 = create(:weam, :no_mailing_address)
 
       addresses = []
-      described_class.add_weams_mailing_addresses(addresses, [weam_1, weam], [])
+      described_class.add_weams_mailing_addresses(addresses, [weam1, weam], [])
       expect(addresses.count).to eq(1)
     end
 
-    it "does not includes a weam row without physical address when facility code was already grabbed" do
+    it 'does not includes a weam row without physical address when facility code was already grabbed' do
       weam = create(:weam, :mailing_address)
-      weam_1 = create(:weam, :no_mailing_address)
+      weam1 = create(:weam, :no_mailing_address)
 
       addresses = []
-      described_class.add_weams_mailing_addresses(addresses, [weam_1, weam],
-                                                   [weam.facility_code])
+      described_class.add_weams_mailing_addresses(addresses, [weam1, weam],
+                                                  [weam.facility_code])
       expect(addresses.count).to eq(0)
     end
   end
