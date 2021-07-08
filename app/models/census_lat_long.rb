8031ef644e7c9155d4cfc0f3767ead81c93dc596
Matt Roth <roth_matthew@bah.com>
2021-07-08 09:32:31 -0400

    Update census_lat_long.rb

--- app/models/census_lat_long.rb
+++ app/models/census_lat_long.rb
@@ -2,7 +2,14 @@
 
 class CensusLatLong < ImportableRecord
   CSV_CONVERTER_INFO = {
-    'record_id_number' => { column: :facility_code, converter: FacilityCodeConverter }
+      'facility_code' => { column: :facility_code, converter: BaseConverter },
+      'input_address' => { column: :input_address, converter: BaseConverter },
+      'tiger_address_range_match_indicator' => { column: :tiger_address_range_match_indicator, converter: BaseConverter },
+      'tiger_match_type' => { column: :tiger_match_type, converter: BaseConverter },
+      'tiger_output_address' => { column: :tiger_output_address, converter: BaseConverter },
+      'interpolated_longitude_latitude' => { column: :interpolated_longitude_latitude, converter: BaseConverter },
+      'tiger_line_id' => { column: :tiger_line_id, converter: BaseConverter },
+      'tiger_line_id_side' => { column: :tiger_line_id_side, converter: BaseConverter }
   }.freeze
 
   # Creates a ZIP file of CSVs by combining results from
