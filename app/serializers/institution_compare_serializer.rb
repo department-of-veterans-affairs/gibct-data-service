66656e7833e45e583b2edb693822781755c2d008
Devin McCurdy <mccurdy_devin@bah.com>
2021-08-12 15:21:50 -0400

    Add fields to institution compare serializer (#855)

--- app/serializers/institution_compare_serializer.rb
+++ app/serializers/institution_compare_serializer.rb
@@ -87,6 +87,9 @@ class InstitutionCompareSerializer < ActiveModel::Serializer
   attribute :hcm2
   attribute :pctfloan
   attribute :institution_category_ratings
+  attribute :school_provider
+  attribute :vet_tec_provider
+  attribute :employer_provider
 
   def yellow_ribbon_programs
     object.yellow_ribbon_programs.map do |yrp|
