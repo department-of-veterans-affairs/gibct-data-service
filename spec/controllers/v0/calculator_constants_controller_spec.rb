06637a58ae3351e1ecd26ba547894a11c9953e85
Patrick Jason Arthur <85634623+PJart@users.noreply.github.com>
2021-12-23 07:13:23 -0800

    Rails 6.1 brakeman upgrade (#872)
    * added gemfile and a few spec changes
    
    * moved rubocop versions down due to large number of requested changes
    
    * moved bundler down
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue
    
    * testing if adding rubyracer gem fixes build issue

--- spec/controllers/v0/calculator_constants_controller_spec.rb
+++ spec/controllers/v0/calculator_constants_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe V0::CalculatorConstantsController, type: :controller do
       create_list(:calculator_constant, 2)
       create(:version, :production)
       get :index
-      expect(response.content_type).to eq('application/json')
+      expect(response.media_type).to eq('application/json')
       expect(response).to match_response_schema('calculator_constants')
     end
   end
