5492134085437a84ff5a15dd52b5ef9f619e715b
Patrick Jason Arthur <85634623+PJart@users.noreply.github.com>
2022-09-02 05:49:46 -0700

    added ability to add ownsership to instiution via dashboard (#924)

--- app/models/institution_owner.rb
+++ app/models/institution_owner.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
-class InstitutionOwner < ApplicationRecord
+class InstitutionOwner < ImportableRecord
+  COLS_USED_IN_INSTITUTION = %i[chief_officer ownership_name].freeze
+
+  CSV_CONVERTER_INFO = {
+    'facility_code' => { column: :facility_code, converter: FacilityCodeConverter },
+    'institution_name' => { column: :institution_name, converter: InstitutionConverter },
+    'chief_officer' => { column: :chief_officer, converter: BaseConverter },
+    'ownership_name' => { column: :ownership_name, converter: BaseConverter }
+  }.freeze
+
+  validates :facility_code, :institution_name, presence: true
 end
