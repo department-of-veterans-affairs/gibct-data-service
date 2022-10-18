28b5b4b45778327468713b55512a9a8abaf3dd78
nfstern02 <Noah.Stern@va.gov>
2022-10-18 12:27:32 -0500

    VFEP-96 #Change rubocop flagged lines to have a space after opening bracket and before closing bracket

--- app/models/archiver.rb
+++ app/models/archiver.rb
@@ -39,8 +39,8 @@ module Archiver
     return if archive.blank?
 
     cols = archive.column_names
-    insert_cols = (cols.map {|col| '"' + col + '"'}).join(', ')
-    select_cols = (cols.map {|col| 's.' + col}).join(', ')
+    insert_cols = (cols.map { |col| '"' + col + '"' }).join(', ')
+    select_cols = (cols.map { |col| 's.' + col }).join(', ')
 
     str = "INSERT INTO #{archive.table_name}(#{insert_cols}) SELECT #{select_cols} FROM #{source.table_name} s "
     str += source.has_attribute?(:institution_id) ? 'JOIN institutions i ON s.institution_id = i.id JOIN versions v ON i.version_id = v.id' : 'JOIN versions v ON s.version_id = v.id'
