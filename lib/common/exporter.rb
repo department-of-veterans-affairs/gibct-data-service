215f0148231ee633917382b6042a2a5dfbfaedc7
nfstern02 <Noah.Stern@va.gov>
2023-07-24 17:49:22 -0500

    fix rubocop issue

--- lib/common/exporter.rb
+++ lib/common/exporter.rb
@@ -31,7 +31,7 @@ module Common
         p[6] = format_ope(p[6])
         p[8] = format_ope(p[8])
       end
-      
+
       partials.prepend(
         ['# GI Bill Students', 'Institution Name', 'Facility code', 'Weams IPEDS', 'Weams OPE', 'Ipeds IPEDS',
          'Ipeds OPE', 'Crosswalk IPEDS', 'Crosswalk OPE']
