652bacd4a8beb90e3acbe1e37f2ce50943aa7708
gfpgcio <gregg.puhala@va.gov>
2023-10-18 13:19:19 -0700

    fix rubocop error

--- app/models/converters/facility_code_converter.rb
+++ app/models/converters/facility_code_converter.rb
@@ -3,9 +3,11 @@
 # Right justifies facility_code to 8 characters using 0s and ensures uppercase.
 class FacilityCodeConverter < BaseConverter
   def self.convert(value)
+    # rubocop:disable Style/IfUnlessModifier
     if value
       value = super(value.to_s).gsub('-', '')
     end
+    # rubocop:enable Style/IfUnlessModifier
     value.blank? ? nil : value.upcase.rjust(8, '0')
   end
 end
