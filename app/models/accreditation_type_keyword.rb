fe5c681d6b66b1bd4e28d5ae1dfd1582c6ff059a
Gregg P <117232882+GcioGregg@users.noreply.github.com>
2023-12-13 15:03:47 -0800

    Minor fixes and tests to accreditatiion type keyword functionality (#1017)
    Co-authored-by: nfstern02 <Noah.Stern@va.gov>

--- app/models/accreditation_type_keyword.rb
+++ app/models/accreditation_type_keyword.rb
@@ -6,8 +6,16 @@ class AccreditationTypeKeyword < ApplicationRecord
   # Order matters. The user should be asked about where to add any new ones.
   ACCREDITATION_TYPES = %w[regional national hybrid].freeze
 
+  before_validation :downcase_keyword
+
   validates :accreditation_type, presence: true
   validates :accreditation_type, inclusion: { in: ACCREDITATION_TYPES }
   validates :keyword_match, presence: true
   validates :keyword_match, uniqueness: { scope: :accreditation_type }
+
+  private
+
+  def downcase_keyword
+    keyword_match.downcase! if keyword_match.present?
+  end
 end
