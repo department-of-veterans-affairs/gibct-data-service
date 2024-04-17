6b2eca83222c3c1454ee5768b01271d5bd927025
Ryan McNeil <ryan.mcneil@oddball.io>
2024-04-17 08:59:43 -0600

    replace docker-compose and fix makefile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# TODO: Remove me after Dockerfile is replaced by Dockerfile-k8s, which points 
+# to /bin/docker-entrypoint
+
 # note this logic is duplciated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
