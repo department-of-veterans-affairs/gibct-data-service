fe3356c56f0e23bd589cf9d0d28d617b1012c86b
Gregg P <117232882+GcioGregg@users.noreply.github.com>
2024-05-06 11:03:33 -0700

    Vfep 1364 - add API's for IpedsIcAy, IpedsIcPy & IpedsIc (#1114)
    * add api for ipedsicay
    
    * apis for ipedsid, ipedsicay, and ipedsicpy

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -171,6 +171,9 @@ class DashboardsController < ApplicationController
         file = "tmp/#{params[:csv_type]}s.csv"
         file = 'tmp/InstitutionCampus.csv' if class_nm.eql?('AccreditationInstituteCampus')
         file = 'tmp/hd2022.csv' if class_nm.eql?('IpedsHd')
+        file = 'tmp/ic2022_ay.csv' if class_nm.eql?('IpedsIcAy')
+        file = 'tmp/ic2022_py.csv' if class_nm.eql?('IpedsIcPy')
+        file = 'tmp/ic2022.csv' if class_nm.eql?('IpedsIc')
 
         upload.csv = file
         file_options = {
@@ -206,6 +209,19 @@ class DashboardsController < ApplicationController
       _stdout, _stderr, status = Open3.capture3("curl -X GET \
       https://nces.ed.gov/ipeds/datacenter/data/HD2022.zip \
         -H 'Content-Type: application/json' -o tmp/download.zip")
+    when klass.name.eql?('IpedsIcAy')
+      _stdout, _stderr, status = Open3.capture3("curl -X GET \
+      https://nces.ed.gov/ipeds/datacenter/data/IC2022_AY.zip \
+        -H 'Content-Type: application/json' -o tmp/download.zip")
+    when klass.name.eql?('IpedsIcPy')
+      _stdout, _stderr, status = Open3.capture3("curl -X GET \
+      https://nces.ed.gov/ipeds/datacenter/data/IC2022_PY.zip \
+        -H 'Content-Type: application/json' -o tmp/download.zip")
+    when klass.name.eql?('IpedsIc')
+      _stdout, _stderr, status = Open3.capture3("curl -X GET \
+      https://nces.ed.gov/ipeds/datacenter/data/IC2022.zip \
+        -H 'Content-Type: application/json' -o tmp/download.zip")
+
     end
     # rubocop:enable Style/EmptyCaseCondition
     status.success?
