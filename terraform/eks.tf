f74482b8b8bf98cf5bcee58cdefdb1558eb26b9c
Ryan McNeil <ryan.mcneil@oddball.io>
2024-05-24 14:58:17 -0600

    source eks cluster from vsp module

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,70 +1,9 @@
-# Taken from https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+module "eks_cluster" {
+  source = "git::ssh://git@github.com/department-of-veterans-affairs/vsp-platform-infrastructure.git//terraform/modules/eks-cluster?ref=eks-cluster-v0.1.2"
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  vpc_id             = module.join_existing_network.vpc.id
+  subnet_ids         = module.join_existing_network.subnets.ids
+  kubernetes_version = var.kubernetes_version
 
-  cluster_name    = "my-cluster"
-  cluster_version = "1.29"
-
-  cluster_endpoint_public_access  = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = var.vpc_id
-  subnet_ids               = []
-  control_plane_subnet_ids = []
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
-    example = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-    }
-  }
-
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::123456789012:role/something"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  context = module.context.context
 }
