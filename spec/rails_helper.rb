98fd2a620c39ee0f29812b9631c9195a09cb0d41
GcioGregg <gregg.puhala@va.gov>
2024-05-26 14:07:27 -0700

    fix rubocop error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  #config.use_transactional_fixtures = false
+  # config.use_transactional_fixtures = false
 
   # Allow short form of FactoryBot calls.
   config.include FactoryBot::Syntax::Methods
