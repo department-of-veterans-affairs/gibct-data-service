b934eeafc9afbb1c3cc929c22bf47013816818b4
nfstern02 <Noah.Stern@va.gov>
2024-06-05 14:27:54 -0500

    cosmetic change

--- app/utilities/caution_flag_templates/hcm_caution_flag.rb
+++ app/utilities/caution_flag_templates/hcm_caution_flag.rb
@@ -5,8 +5,8 @@ require_relative 'caution_flag_template'
 class CautionFlagTemplates::HcmCautionFlag < CautionFlagTemplates::CautionFlagTemplate
   NAME = Hcm.name
   TITLE = 'School placed on Heightened Cash Monitoring'
-  DESCRIPTION = 'The Department of Education has placed this '\
-                'school on Heightened Cash Monitoring because of financial or federal compliance issues.'
+  DESCRIPTION = 'The Department of Education has placed this school on '\
+                'Heightened Cash Monitoring because of financial or federal compliance issues.'
   LINK_TEXT = 'Learn more about Heightened Cash Monitoring'
   LINK_URL = 'https://studentaid.gov/data-center/school/hcm'
 end
