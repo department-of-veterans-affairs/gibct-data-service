aadbb92068b1264c843d99370caa7751c463a04f
Gregg P <117232882+GcioGregg@users.noreply.github.com>
2024-06-13 08:47:48 -0700

    Vfep 1534 docker change restore to 7.0.8.1 (#1163)
    * api for Hcm
    
    * clean up code
    
    * api for eight_key
    
    * moved csv conversion method to bottom
    
    * update gemfile.lock to address blocking issue with recent Rexml upgrade
    
    * added comment
    
    * fixed eight_key IF structure
    
    * upgrade rails to 7.1.3
    
    * removed 7.1.3 upgrade notes file
    
    * upgrade rails to 7.1.3.4
    
    * udpate dockerfile to ruby 3.3.1
    
    * change to terser from Uglifier
    
    * fix security issue
    
    * fix regex reference
    
    * add 443 to regex
    
    * fix origin security issue
    
    * fix origin
    
    * clean up origin regex
    
    * restore to rails 7.0.8.1
    
    * restore to 7.0.8.1
    
    * remove terser gem
    
    ---------
    
    Co-authored-by: nfstern02 <72567812+nfstern02@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-gem 'rails', '7.1.3.4'
+gem 'rails', '7.0.8.1'
 
 gem 'active_model_serializers', '~> 0.10.14' # JSON API
 gem 'activerecord-import' # Mass importing of CSV data
@@ -42,7 +42,6 @@ gem 'sentry-raven', '~> 3.1.2'
 gem 'sitemap_generator'
 gem 'sprockets-rails' # Rails 7 upgrade - needed for now.
 gem 'strong_migrations'
-gem 'terser'
 gem 'uglifier', '>= 1.3.0'
 gem 'vets_json_schema', git: 'https://github.com/department-of-veterans-affairs/vets-json-schema', branch: 'master'
 gem 'virtus', '~> 2.0.0'
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -10,11 +10,6 @@ module GibctDataService
   class Application < Rails::Application
     config.load_defaults '7.0' # enables zeitwerk mode in CRuby
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
     # Configuration for the application, engines, and railties goes here.
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
@@ -25,9 +20,8 @@ module GibctDataService
     # CORS configuration; see also cors_preflight route
     config.middleware.insert_before 0, Rack::Cors, logger: (-> { Rails.logger }) do
       allow do
-        regex = Regexp.new(Settings.web_origin_regex)
-        origins { |source, _env| Settings.web_origin.split(',').include?(source) || source.match?(regex) }
-        resource '*', headers: :any, methods: :any, credentials: true
+        origins 'localhost:3001', 'localhost:3000', 'localhost'
+        resource '/v0/*', headers: :any, methods: :any, credentials: true      
       end
     end
 
@@ -48,7 +42,6 @@ module GibctDataService
 
     # Rails 7 upgrade - turn off warnings
     # https://stackoverflow.com/questions/76347365/how-do-i-set-legacy-connection-handling-to-false-in-my-rails-application
-    # the legacy_connection_handling configuration option, which was deprecated in Rails 7.0 and removed in Rails 7.1
-    # config.active_record.legacy_connection_handling = false
+    config.active_record.legacy_connection_handling = false
   end
 end
