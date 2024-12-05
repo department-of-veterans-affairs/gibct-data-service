4470d434200f524fba7f01398b50369b50642de1
Ben Allen <ben.allen@oddball.io>
2024-12-05 08:21:43 -0400

    Move Terraform to https://github.com/department-of-veterans-affairs/terraform-aws-vsp-gids

--- terraform/subnet.tf
+++
@@ -1,7 +0,0 @@
-data "aws_subnet" "selected" {
-  count = length(var.vpc_subnets)
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_subnets[count.index]]
-  }
-}
--- terraform/vpc.tf
+++
@@ -1,6 +0,0 @@
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc]
-  }
-}
