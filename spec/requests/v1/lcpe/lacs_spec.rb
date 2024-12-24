803dc2dee5c5c817da9c52ebb3568f69a6bb1cf1
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-12-24 12:18:52 -0600

    LCPE: added controllers

---
+++ spec/requests/v1/lcpe/lacs_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# RSpec.describe "V1::Lcpe::Lacs", type: :request do
+#   describe "GET /index" do
+#     it "returns http success" do
+#       get "/v1/lcpe/lacs/index"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /show" do
+#     it "returns http success" do
+#       get "/v1/lcpe/lacs/show"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+# end
