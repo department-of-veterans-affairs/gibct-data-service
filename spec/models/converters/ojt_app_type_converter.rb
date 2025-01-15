3fc64c08b28526eafa546a2179ba14824a87386a
Jeff Marks <jeff.t.marks@gmail.com>
2025-01-15 12:35:16 -0700

    Create spec for converter

---
+++ spec/models/converters/ojt_app_type_converter.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Converters::OjtAppTypeConverter do
+  subject { described_class }
+
+  # Accepted characters: G, K, P, E
+ 
+  it 'converts lowercase matched character into abbreviated OJT/APP type' do
+    expect(described_class.convert('k')).to eq('APP')
+  end
+
+  it 'converts upcase matched character into abbreviated OJT/APP type' do
+    expect(described_class.convert('K')).to eq('APP')
+  end
+
+  it 'returns nil if the value is not a matched character' do
+    expect(described_class.convert('Z')).to be_nil
+    expect(described_class.convert(nil)).to be_nil
+  end
+end
