4a82269c48f103614ed11dfb18530d078eb29be0
Jeff Marks <jeff.t.marks@gmail.com>
2025-01-29 10:47:55 -0700

    Complete upload file processor spec

--- spec/jobs/process_upload_job.spec.rb
+++ spec/jobs/process_upload_job.spec.rb
@@ -9,10 +9,13 @@ RSpec.describe ProcessUploadJob, type: :job do
     let(:upload) { create(:async_upload, :with_blob, user: user, status_message: status)}
     let(:klass) { upload.csv_type.constantize }
     let(:status) { 'queued for upload' }
+    let(:upload_processor) { instance_double("UploadFileProcessor") }
 
     context 'with successful database import' do
-      it 'loads rows from blob into database' do
-        expect { job.perform(upload) }.to change(klass, :count).by(row_count)
+      it 'calls UploadFileProcessor#load_file' do
+        allow(UploadFileProcessor).to receive(:new).with(upload).and_return(upload_processor)
+        expect(upload_processor).to receive(:load_file)
+        job.perform(upload)
       end
 
       it 'saves alerts as json to upload#status_message' do
@@ -23,7 +26,6 @@ RSpec.describe ProcessUploadJob, type: :job do
 
     context 'with failed database import' do
       it 'cancels upload' do
-        upload_processor = instance_double("UploadFileProcessor")
         allow(UploadFileProcessor).to receive(:new).with(upload).and_return(upload_processor)
         allow(upload_processor).to receive(:load_file).and_raise(StandardError)
         expect(upload).to receive(:cancel!)
