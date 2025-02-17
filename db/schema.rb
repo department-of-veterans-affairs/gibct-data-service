981ee5fed35ffd8f079b59c4ee6253f30183d3d2
Jeff Marks <jeff.t.marks@gmail.com>
2025-02-17 07:49:49 -0700

    Revert migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_14_015743) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_27_081700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "cube"
   enable_extension "earthdistance"
@@ -1443,13 +1443,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_015743) do
     t.index ["lac_nm"], name: "lcpe_lacs_lac_nm_idx"
   end
 
-  create_table "lcpe_preload_datasets", force: :cascade do |t|
-    t.text "body"
-    t.string "subject_class"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "lcpe_preloads", force: :cascade do |t|
     t.string "lcpe_type"
     t.text "data"
@@ -1771,10 +1764,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_015743) do
     t.datetime "updated_at", precision: nil, null: false
     t.datetime "completed_at", precision: nil
     t.boolean "multiple_file_upload", default: false
-    t.string "blob"
-    t.string "status_message"
-    t.datetime "queued_at", precision: nil
-    t.datetime "canceled_at", precision: nil
     t.index ["csv_type"], name: "index_uploads_on_csv_type"
     t.index ["updated_at"], name: "index_uploads_on_updated_at"
     t.index ["user_id"], name: "index_uploads_on_user_id"
