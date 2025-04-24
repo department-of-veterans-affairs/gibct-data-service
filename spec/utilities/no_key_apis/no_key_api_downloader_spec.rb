fd57c95f9568f1f62f4285be2a510026bf6c0ce7
Jeff Marks <jeff.t.marks@gmail.com>
2025-04-24 14:48:13 -0600

    Downloader spec

--- spec/utilities/no_key_apis/no_key_api_downloader_spec.rb
+++ spec/utilities/no_key_apis/no_key_api_downloader_spec.rb
@@ -7,6 +7,12 @@ require 'rails_helper'
 # calling, but it's advisable to run it from the command line separately and not as part of the test suite
 
 RSpec.describe NoKeyApis::NoKeyApiDownloader do
+  let(:ipeds_page) { File.read('spec/fixtures/ipeds_directory_page.txt') }
+  let(:nokogiri_doc) { Nokogiri::HTML(ipeds_page) }
+  let(:scraper) { instance_double(NoKeyApis::WebScraper, scrape: nokogiri_doc) }
+
+  before { allow(NoKeyApis::WebScraper).to receive(:new).and_return(scraper) }
+
   describe '#initialize' do
     %w[Accreditation AccreditationAction AccreditationInstituteCampus AccreditationRecord].each do |class_nm|
       it "sets the accreditation curl command correctly for #{class_nm}" do
@@ -34,7 +40,7 @@ RSpec.describe NoKeyApis::NoKeyApiDownloader do
         expect(nkad.class_nm).to eq(class_nm)
         expect(nkad.curl_command).to include('tmp/download.zip')
         expect(nkad.curl_command).to include('-X GET')
-        expect(nkad.curl_command).to include('https://nces.ed.gov/ipeds/datacenter/data')
+        expect(nkad.curl_command).to include('https://nces.ed.gov/ipeds/datacenter/data/IC2023_AY.zip')
         expect(nkad.curl_command).not_to include('-d')
       end
     end
@@ -62,6 +68,21 @@ RSpec.describe NoKeyApis::NoKeyApiDownloader do
     end
   end
 
+  # We obtain the download source for certain file types by scraping web page for most recent download link
+  describe 'dynamic download sources' do
+    describe '.fetch_ipeds_source_for' do
+      %w[IpedsHd IpedsIc IpedsIcAy IpedsIcPy].each do |class_nm|
+        it "iterates through html table and determines most recent download link for #{class_nm}" do
+          matcher = described_class::IPEDS_MATCHERS[class_nm]
+          link_tags = nokogiri_doc.css('.idc_gridviewrow td a').select { |link| link.text.match?(matcher) }
+          expect(link_tags.size).to be > 1
+          most_recent_link = described_class.send(:fetch_ipeds_source_for, class_nm)
+          expect(most_recent_link).to include '2023'
+        end
+      end
+    end
+  end
+
   # Run this guy if you want to test the downloading making the remote calls. This can be done from the
   # command line as follows
   # EXTERNAL=true rspec spec/utilities/no_key_apis/no_key_api_downloader_spec.rb:line_number_of_describe
@@ -71,11 +92,11 @@ RSpec.describe NoKeyApis::NoKeyApiDownloader do
     call_externally = ENV['EXTERNAL'].eql?('true') ? true : false
     context 'when making external calls', if: call_externally do
       it 'downloads the hcm file into the tmp folder' do
-        system('rm tmp/hcm.xlsx') if File.exist?('tmp/hcm.xlsx')
-        expect(File).not_to exist('tmp/hcm.xlsx')
+        system('rm tmp/hcm.xls') if File.exist?('tmp/hcm.xls')
+        expect(File).not_to exist('tmp/hcm.xls')
         nkad = described_class.new('Hcm')
         nkad.download_csv
-        expect(File).to exist('tmp/hcm.xlsx')
+        expect(File).to exist('tmp/hcm.xls')
       end
 
       it 'downloads the eight key file into the tmp folder' do
