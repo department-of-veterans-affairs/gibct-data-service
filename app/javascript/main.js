1508f7c0fc485abd70c62b7ae63ef0761955f52f
Jeff Marks <jeff.t.marks@gmail.com>
2025-06-12 10:04:21 -0600

    Configure importmap and hotwire stimulus/turbo

---
+++ app/javascript/main.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Renamed from application.js to main.js to avoid name collision with legacy app/assets/javascripts/application.js
+
+import { Application } from "@hotwired/stimulus";
+import "@hotwired/turbo-rails"
+// import ColasFormController from "./controllers/colas_form_controller";
+// import ConstantsFormController from "./controllers/constants_form_controller";
+
+window.Stimulus = Application.start();
+// Stimulus.register("colas-form", ColasFormController);
+// Stimulus.register("constants-form", ConstantsFormController);
