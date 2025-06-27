d0c7c30772a81fc3bc2af0ab9053a7aa478d702d
Jeff Marks <jeff.t.marks@gmail.com>
2025-06-27 08:41:24 -0600

    Fix typo

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,4 @@
 //= link application.css
 //= link application.js
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
