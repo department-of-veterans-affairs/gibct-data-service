8c8c037c120bf99e1725cf91c5d21ed16b1c70f2
Jeff Marks <jeff.t.marks@gmail.com>
2025-07-02 10:08:07 -0600

    remove old migrations

--- db/migrate/20250612181022_add_rate_adjustment_reference_to_calculator_constants.rb
+++
@@ -1,8 +0,0 @@
-class AddRateAdjustmentReferenceToCalculatorConstants < ActiveRecord::Migration[7.1]
-  # disable_ddl_transaction!
-
-  def change
-    # add_column :calculator_constants, :previous_year, :float
-    add_reference :calculator_constants, :rate_adjustment, index: { algorithm: :concurrently }
-  end
-end
