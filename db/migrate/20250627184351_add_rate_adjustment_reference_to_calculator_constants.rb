82977a6fe3037656761a4e228ef67f39a470a062
Jeff Marks <jeff.t.marks@gmail.com>
2025-07-02 14:21:28 -0600

    Delete migrations before merge

--- db/migrate/20250627184351_add_rate_adjustment_reference_to_calculator_constants.rb
+++
@@ -1,7 +0,0 @@
-class AddRateAdjustmentReferenceToCalculatorConstants < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-  
-  def change
-    add_reference :calculator_constants, :rate_adjustment, index: { algorithm: :concurrently }
-  end
-end
