0e2e891b137ea277075a45a6a2934719e534cbbb
Jeff Marks <jeff.t.marks@gmail.com>
2025-07-03 10:09:16 -0600

    remove comments

--- spec/models/calculator_constant_spec.rb
+++ spec/models/calculator_constant_spec.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-# require 'models/shared_examples/shared_examples_for_loadable'
-# require 'models/shared_examples/shared_examples_for_exportable'
 
 RSpec.describe CalculatorConstant, type: :model do
   # No longer importable record, updated instead via calculator constants dashboard
-  # it_behaves_like 'a loadable model', skip_lines: 0
-  # it_behaves_like 'an exportable model', skip_lines: 0
 
   describe 'instance methods' do
     it 'responds to value' do
