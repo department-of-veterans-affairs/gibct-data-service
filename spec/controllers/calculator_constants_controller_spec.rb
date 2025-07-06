5effb7cdf2d62fab09df2c1e09a0781886869d2b
Jeff Marks <jeff.t.marks@gmail.com>
2025-07-05 18:31:37 -0600

    linting

--- spec/controllers/calculator_constants_controller_spec.rb
+++ spec/controllers/calculator_constants_controller_spec.rb
@@ -8,7 +8,7 @@ require 'controllers/shared_examples/shared_examples_for_collection_updatable'
 
 RSpec.describe CalculatorConstantsController, type: :controller do
   before { CalculatorConstant.delete_all }
-  
+
   it_behaves_like 'an authenticating controller', :index, 'calculator_constants'
 
   describe 'GET #index' do
