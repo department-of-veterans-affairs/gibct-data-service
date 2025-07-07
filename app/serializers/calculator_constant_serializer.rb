d28f492cb68a5e43a9484acefe6d892ce88d091e
Jeff Marks <jeff.t.marks@gmail.com>
2025-07-07 14:24:21 -0600

    fix serializer

--- app/serializers/calculator_constant_serializer.rb
+++ app/serializers/calculator_constant_serializer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class CalculatorConstantSerializer < ActiveModel::Serializer
+  # Override model type
+  type 'calculator_constants'
+
   attributes :name, :value
 
   def value
