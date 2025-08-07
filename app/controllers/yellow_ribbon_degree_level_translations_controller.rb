900f9843dc5b03ddc203cb89333898bd02fad6a0
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-08-07 13:36:00 -0700

    CRUD actions for yellow ribbon degree level dashboard

--- app/controllers/yellow_ribbon_degree_level_translations_controller.rb
+++ app/controllers/yellow_ribbon_degree_level_translations_controller.rb
@@ -16,6 +16,34 @@ class YellowRibbonDegreeLevelTranslationsController < ApplicationController
       render :index, status: :unprocessable_entity
     end
   end
+  
+  def show
+    @translation = YellowRibbonDegreeLevelTranslation.find(params[:id])
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace(@translation, partial: 'yellow_ribbon_degree_level_translations/show_row', locals: { translation: @translation }) }
+    end
+  end
+  
+  def edit
+    @translation = YellowRibbonDegreeLevelTranslation.find(params[:id])
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace(@translation, partial: 'yellow_ribbon_degree_level_translations/edit_row', locals: { translation: @translation }) }
+    end
+  end
+
+  def update
+    @translation = YellowRibbonDegreeLevelTranslation.find(params[:id])
+
+    respond_to do |format|
+      if @translation.update(translation_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@translation, partial: 'yellow_ribbon_degree_level_translations/show_row', locals: { translation: @translation }) }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@translation, partial: 'yellow_ribbon_degree_level_translations/edit_row', locals: { translation: @translation }) }
+      end
+    end
+  end
 
   def destroy
     @translation = YellowRibbonDegreeLevelTranslation.find(params[:id])
