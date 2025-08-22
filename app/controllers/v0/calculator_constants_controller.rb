41d9894ebd9d7f6def395c5d39cc88699b7f7fe8
Jeff Marks <jeff.t.marks@gmail.com>
2025-08-22 14:10:05 -0600

    remove toggles

--- app/controllers/v0/calculator_constants_controller.rb
+++ app/controllers/v0/calculator_constants_controller.rb
@@ -4,23 +4,14 @@ module V0
   class CalculatorConstantsController < ApiController
     # GET /v0/calculator/constants
     def index
-      if CalculatorConstant.versioning_enabled?
-        # Use the Versioned CalculatorConstant model to get the data
-        @data = CalculatorConstantVersion.where(version: @version)
-        @links = { self: self_link }
-        @meta = { version: @version }
-        render json: @data,
-               each_serializer: CalculatorConstantSerializer,
-               meta: @meta,
-               links: @links
-      else
-        @data = CalculatorConstant.all
-        @links = { self: self_link }
-        render json: @data,
-               each_serializer: CalculatorConstantSerializer,
-               meta: { version: @version },
-               links: @links
-      end
+      # Use the Versioned CalculatorConstant model to get the data
+      @data = CalculatorConstantVersion.where(version: @version)
+      @links = { self: self_link }
+      @meta = { version: @version }
+      render json: @data,
+              each_serializer: CalculatorConstantSerializer,
+              meta: @meta,
+              links: @links
     end
   end
 end
