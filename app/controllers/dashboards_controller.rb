81f3ffc4afdf9cc6c0e5f2ca4c344499bccdfdb5
Jeff Marks <jeff.t.marks@gmail.com>
2025-08-22 19:47:37 -0600

    linting

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,7 +10,7 @@ class DashboardsController < ApplicationController
 
   def build
     GeneratePreviewJob.perform_later(current_user)
-    psgi = PreviewGenerationStatusInformation.create!(current_progress: 'Preview Version being generated.')
+    PreviewGenerationStatusInformation.create!(current_progress: 'Preview Version being generated.')
 
     redirect_to dashboards_path
   end
