988140b58930b74ae1191003728bed2f2fa5d44b
Brandon Cruz <brandon@adhocteam.us>
2026-01-26 18:14:27 -0500

    feat: Update Docker setup and dependencies for improved config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,7 @@
 # if you make major alteration here, please check that usage as well
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
 
+# Remove stale PID file if it exists
+rm -f tmp/pids/server.pid
+
 exec "$@"
