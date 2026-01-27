90da521e16d260f27cd5509aaf3da461a0bae0df
Brandon Cruz <brandon@adhocteam.us>
2026-01-26 19:05:48 -0500

    Remove binstubs option from bundle install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # note this logic is duplciated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
+bundle check || bundle install
 
 # Remove stale PID file if it exists
 rm -f tmp/pids/server.pid
